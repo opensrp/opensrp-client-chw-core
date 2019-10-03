26ee8ac7dbe80c4c9dd96a67845b91772b847af5
paulinembabu <pauline.mbabu@gmail.com>
2019-10-03 16:59:42 +0300

    Incremented gradle files and Fixed Birth Certificate Date Constraint

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CoreChildHomeVisitActivity.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CoreChildHomeVisitActivity.java
@@ -7,41 +7,77 @@ import android.content.Intent;
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 import com.vijay.jsonwizard.domain.Form;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.chw.anc.activity.BaseAncHomeVisitActivity;
 import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.core.R;
+import org.smartregister.chw.core.utils.CoreJsonFormUtils;
 import org.smartregister.family.util.Constants;
 import org.smartregister.family.util.JsonFormUtils;
 import org.smartregister.family.util.Utils;
 import org.smartregister.util.LangUtils;
 
 import java.text.MessageFormat;
+import java.util.Date;
+
+import timber.log.Timber;
 
 public abstract class CoreChildHomeVisitActivity extends BaseAncHomeVisitActivity {
+    private Date dob;
 
     public static void startMe(Activity activity, MemberObject memberObject, Boolean isEditMode, Class<?> cls) {
         Intent intent = new Intent(activity, cls);
         intent.putExtra(org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.MEMBER_PROFILE_OBJECT, memberObject);
         intent.putExtra(org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.EDIT_MODE, isEditMode);
         activity.startActivityForResult(intent, org.smartregister.chw.anc.util.Constants.REQUEST_CODE_HOME_VISIT);
+        String date = memberObject.getDob();
     }
 
     @Override
     protected abstract void registerPresenter();
 
+
+    private void updateFormField(JSONArray formFieldArrays, String formFieldKey, Date updateValue) {
+        if (updateValue != null) {
+            JSONObject formObject = org.smartregister.util.JsonFormUtils.getFieldJSONObject(formFieldArrays, formFieldKey);
+            if (formObject != null) {
+                try {
+                    formObject.remove(org.smartregister.util.JsonFormUtils.VALUE);
+                    formObject.put(org.smartregister.util.JsonFormUtils.VALUE, updateValue);
+                } catch (JSONException e) {
+                    Timber.e(e);
+                }
+            }
+        }
+    }
+
     @Override
     public void startFormActivity(JSONObject jsonForm) {
+        try {
 
-        Form form = new Form();
-        form.setActionBarBackground(R.color.family_actionbar);
-        form.setWizard(false);
+            JSONObject stepOne = jsonForm.getJSONObject(JsonFormUtils.STEP1);
+            JSONArray jsonArray = stepOne.getJSONArray(JsonFormUtils.FIELDS);
+            if (memberObject.getDob() != null) {
+                JSONObject min_date = CoreJsonFormUtils.getFieldJSONObject(jsonArray, "birth_cert_issue_date");
+                int days = CoreJsonFormUtils.getDayFromDate(memberObject.getDob());
 
-        Intent intent = new Intent(this, Utils.metadata().familyMemberFormActivity);
-        intent.putExtra(org.smartregister.family.util.Constants.JSON_FORM_EXTRA.JSON, jsonForm.toString());
-        intent.putExtra(Constants.WizardFormActivity.EnableOnCloseDialog, false);
-        intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
-        startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
+                min_date.put("min_date", "today-" + days + "d");
+            }
+            Form form = new Form();
+            form.setActionBarBackground(R.color.family_actionbar);
+            form.setWizard(false);
+
+            Intent intent = new Intent(this, Utils.metadata().familyMemberFormActivity);
+            intent.putExtra(org.smartregister.family.util.Constants.JSON_FORM_EXTRA.JSON, jsonForm.toString());
+            intent.putExtra(Constants.WizardFormActivity.EnableOnCloseDialog, false);
+            intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
+            startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+
     }
 
     @Override
