80a9b6890c018f3895d4c90ef53dfcb08fdda84e
Allan O <a.onchuru@gmail.com>
2019-10-04 19:47:19 +0300

    Refactor getPncCommonPersonObject
    Removed separately getting PNC columns of interest since we'll be getting them using PNC Dao

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/repository/PncRegisterRepository.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/repository/PncRegisterRepository.java
@@ -19,44 +19,12 @@ import timber.log.Timber;
 public class PncRegisterRepository extends BaseRepository {
 
     public static final String TABLE_NAME = CoreConstants.TABLE_NAME.PNC_MEMBER;
-    public static final String BASE_ENTITY_ID = "base_entity_id";
-    private static final String PREGNANCY_OUTCOME = "preg_outcome";
-    private static final String IS_CLOSED = "is_closed";
-    private static final String DELIVERY_DATE = "delivery_date";
-    private static final String LAST_VISIT_DATE = "last_visit_date";
-    private static final String RELATIONALID = "relationalid";
-    private static final String[] PNC_COUNT_TABLE_COLUMNS = {"id as _id", BASE_ENTITY_ID, IS_CLOSED, DELIVERY_DATE, LAST_VISIT_DATE, PREGNANCY_OUTCOME, RELATIONALID};
 
     public PncRegisterRepository(Repository repository) {
         super(repository);
     }
 
     public CommonPersonObject getPncCommonPersonObject(String baseEntityId) {
-        SQLiteDatabase database = getReadableDatabase();
-        Cursor cursor = null;
-        CommonPersonObject personObject = null;
-        try {
-            if (database == null) {
-                return null;
-            }
-            String selection = DBConstants.KEY.BASE_ENTITY_ID + " = ? " + COLLATE_NOCASE + " AND " +
-                    DBConstants.KEY.IS_CLOSED + " = ? " + COLLATE_NOCASE;
-            String[] selectionArgs = new String[]{baseEntityId, "0"};
-            cursor = database.query(CoreConstants.TABLE_NAME.PNC_MEMBER, PNC_COUNT_TABLE_COLUMNS, selection, selectionArgs, null, null, null);
-            if (cursor != null && cursor.getCount() > 0 && cursor.moveToFirst()) {
-                personObject = Utils.context().commonrepository(TABLE_NAME).readAllcommonforCursorAdapter(cursor);
-            }
-        } catch (Exception e) {
-            Timber.e(e);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-        return personObject;
-    }
-
-    public CommonPersonObject getPncFamilyDetailsCommonPersonObject(String baseEntityId) {
         SQLiteDatabase database = getReadableDatabase();
         Cursor cursor = null;
         CommonPersonObject personObject = null;
