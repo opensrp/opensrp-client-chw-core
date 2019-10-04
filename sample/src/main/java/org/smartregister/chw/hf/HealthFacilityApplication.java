0a6530f3d1e1588f2914bd13067431f3138d5081
Elly Kitoto <junkmailstoelly@gmail.com>
2019-10-04 15:37:01 +0300

    add all clients to navigation menu

--- sample/src/main/java/org/smartregister/chw/hf/HealthFacilityApplication.java
+++ sample/src/main/java/org/smartregister/chw/hf/HealthFacilityApplication.java
@@ -162,7 +162,7 @@ public class HealthFacilityApplication extends CoreChwApplication implements Cor
         registeredActivities.put(CoreConstants.REGISTERED_ACTIVITIES.CHILD_REGISTER_ACTIVITY, ChildRegisterActivity.class);
         registeredActivities.put(CoreConstants.REGISTERED_ACTIVITIES.PNC_REGISTER_ACTIVITY, PncRegisterActivity.class);
         registeredActivities.put(CoreConstants.REGISTERED_ACTIVITIES.REFERRALS_REGISTER_ACTIVITY, ReferralRegisterActivity.class);
-        //registeredActivities.put(CoreConstants.REGISTERED_ACTIVITIES.MALARIA_REGISTER_ACTIVITY, FamilyRegisterActivity.class);
+        registeredActivities.put(CoreConstants.REGISTERED_ACTIVITIES.ALL_CLIENTS_REGISTERED_ACTIVITY, FamilyRegisterActivity.class);
         return registeredActivities;
     }
 
