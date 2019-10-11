8cf61463183211d92d25d3eeeae17fec51b4f860
Elly Kitoto <junkmailstoelly@gmail.com>
2019-10-11 12:28:45 +0300

    change malaria list row design

--- sample/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
+++ sample/src/main/java/org/smartregister/chw/hf/fragment/MalariaRegisterFragment.java
@@ -3,6 +3,8 @@ package org.smartregister.chw.hf.fragment;
 import org.smartregister.chw.core.fragment.CoreMalariaRegisterFragment;
 import org.smartregister.chw.core.model.MalariaRegisterFragmentModel;
 import org.smartregister.chw.hf.presenter.MalariaRegisterFragmentPresenter;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.util.Utils;
 import org.smartregister.view.activity.BaseRegisterActivity;
 
 public class MalariaRegisterFragment extends CoreMalariaRegisterFragment {
@@ -14,6 +16,11 @@ public class MalariaRegisterFragment extends CoreMalariaRegisterFragment {
         }
         String viewConfigurationIdentifier = ((BaseRegisterActivity) getActivity()).getViewIdentifiers().get(0);
         presenter = new MalariaRegisterFragmentPresenter(this, new MalariaRegisterFragmentModel(), viewConfigurationIdentifier);
+    }
+
+    @Override
+    protected void openProfile(CommonPersonObjectClient client) {
+        Utils.showShortToast(getContext(), "Go to malaria profile");
     }
 
 }
