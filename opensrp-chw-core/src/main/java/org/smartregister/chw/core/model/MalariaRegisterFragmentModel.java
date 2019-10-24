7765244760f3634165a395c5d4af033572039c55
Manu Tarus <manutarus@gmail.com>
2019-10-24 18:33:32 +0300

    checks if member is on anc/pnc

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/model/MalariaRegisterFragmentModel.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/model/MalariaRegisterFragmentModel.java
@@ -47,6 +47,8 @@ public class MalariaRegisterFragmentModel extends BaseMalariaRegisterFragmentMod
         columnList.add(CoreConstants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.PRIMARY_CAREGIVER);
         columnList.add(CoreConstants.TABLE_NAME.PNC_MEMBER + "." + ChwDBConstants.DELIVERY_DATE);
         columnList.add(CoreConstants.TABLE_NAME.ANC_MEMBER + "." + org.smartregister.chw.anc.util.DBConstants.KEY.PHONE_NUMBER);
+        columnList.add(CoreConstants.TABLE_NAME.ANC_MEMBER + "." + ChwDBConstants.IS_CLOSED + " as is_anc_closed");
+        columnList.add(CoreConstants.TABLE_NAME.PNC_MEMBER + "." + ChwDBConstants.IS_CLOSED + " as is_pnc_closed");
         columnList.add(CoreConstants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FIRST_NAME + " as " + org.smartregister.chw.anc.util.DBConstants.KEY.FAMILY_NAME);
         return columnList.toArray(new String[columnList.size()]);
     }
