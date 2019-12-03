91900a799ad62b61052863ee1d305c192c131344
Manu Tarus <manutarus@gmail.com>
2019-12-03 16:59:38 +0300

    open fp initiation form form pnc profile

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CorePncMemberProfileActivity.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CorePncMemberProfileActivity.java
@@ -54,10 +54,10 @@ public abstract class CorePncMemberProfileActivity extends BasePncMemberProfileA
             return true;
 
         } else if (itemId == R.id.action_malaria_confirmation) {
-            startFpRegister();
+            startMalariaRegister();
             return true;
         } else if (itemId == R.id.action_fp_initiation) {
-            startMalariaRegister();
+            startFpRegister();
             return true;
         } else if (itemId == R.id.action__pnc_remove_member) {
             removePncMember();
