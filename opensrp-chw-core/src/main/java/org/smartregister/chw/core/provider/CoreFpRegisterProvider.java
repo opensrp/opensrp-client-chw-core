6fffe20d51095e8e15fe7e12cc140eae05f7b3dc
Manu Tarus <manutarus@gmail.com>
2019-12-03 08:27:37 +0300

    move module class to fp module

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/provider/CoreFpRegisterProvider.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/provider/CoreFpRegisterProvider.java
@@ -2,29 +2,12 @@ package org.smartregister.chw.core.provider;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.os.AsyncTask;
 import android.view.View;
-import android.widget.Button;
 
-import org.apache.commons.lang3.StringUtils;
-import org.smartregister.chw.core.R;
-import org.smartregister.chw.core.rule.MalariaFollowUpRule;
-import org.smartregister.chw.core.utils.CoreConstants;
-import org.smartregister.chw.core.utils.MalariaVisitUtil;
 import org.smartregister.chw.fp.provider.BaseFpRegisterProvider;
-import org.smartregister.chw.malaria.provider.MalariaRegisterProvider;
-import org.smartregister.chw.malaria.util.DBConstants;
-import org.smartregister.commonregistry.CommonPersonObjectClient;
-import org.smartregister.util.Utils;
 import org.smartregister.view.contract.SmartRegisterClient;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.Set;
-
-import timber.log.Timber;
 
 public class CoreFpRegisterProvider extends BaseFpRegisterProvider {
 
@@ -42,7 +25,6 @@ public class CoreFpRegisterProvider extends BaseFpRegisterProvider {
 
         viewHolder.dueButton.setVisibility(View.GONE);
         viewHolder.dueButton.setOnClickListener(null);
-        CommonPersonObjectClient pc = (CommonPersonObjectClient) client;
     }
 
 
