8efa50689aeff46cb6e0741ba926a75b38d2472b
Allan O <a.onchuru@gmail.com>
2019-12-13 17:01:25 +0300

    :recycle: Refactor get last visit date to check for the register the member belongs to

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/CoreFamilyPlanningProfileInteractor.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/CoreFamilyPlanningProfileInteractor.java
@@ -1,11 +1,15 @@
 package org.smartregister.chw.core.interactor;
 
+import org.apache.commons.lang3.StringUtils;
 import org.smartregister.chw.anc.AncLibrary;
 import org.smartregister.chw.anc.domain.Visit;
+import org.smartregister.chw.anc.util.Constants;
+import org.smartregister.chw.core.dao.AncDao;
+import org.smartregister.chw.core.dao.PNCDao;
+import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.fp.contract.BaseFpProfileContract;
 import org.smartregister.chw.fp.domain.FpMemberObject;
 import org.smartregister.chw.fp.interactor.BaseFpProfileInteractor;
-import org.smartregister.chw.fp.util.FamilyPlanningConstants;
 import org.smartregister.domain.AlertStatus;
 
 import java.util.Date;
@@ -31,11 +35,38 @@ public class CoreFamilyPlanningProfileInteractor extends BaseFpProfileInteractor
         appExecutors.diskIO().execute(runnable);
     }
 
+    private String getMemberVisitType(String baseEntityId) {
+        String type = null;
+
+        if (AncDao.isANCMember(baseEntityId)) {
+            type = CoreConstants.TABLE_NAME.ANC_MEMBER;
+        } else if (PNCDao.isPNCMember(baseEntityId)) {
+            type = CoreConstants.TABLE_NAME.PNC_MEMBER;
+        }
+        return type;
+    }
+
     private Date getLastVisitDate(FpMemberObject memberObject) {
         Date lastVisitDate = null;
-        Visit lastVisit = AncLibrary.getInstance().visitRepository().getLatestVisit(memberObject.getBaseEntityId(), FamilyPlanningConstants.EventType.FP_HOME_VISIT);
-        if (lastVisit != null) {
-            lastVisitDate = lastVisit.getDate();
+        String visitType = null;
+        String memberType = getMemberVisitType(memberObject.getBaseEntityId());
+
+        switch (memberType) {
+            case CoreConstants.TABLE_NAME.PNC_MEMBER:
+                visitType = Constants.EVENT_TYPE.PNC_HOME_VISIT;
+                break;
+            case CoreConstants.TABLE_NAME.ANC_MEMBER:
+                visitType = Constants.EVENT_TYPE.ANC_HOME_VISIT;
+                break;
+            default:
+                break;
+        }
+
+        if (StringUtils.isNotBlank(visitType)) {
+            Visit lastVisit = AncLibrary.getInstance().visitRepository().getLatestVisit(memberObject.getBaseEntityId(), visitType);
+            if (lastVisit != null) {
+                lastVisitDate = lastVisit.getDate();
+            }
         }
 
         return lastVisitDate;
