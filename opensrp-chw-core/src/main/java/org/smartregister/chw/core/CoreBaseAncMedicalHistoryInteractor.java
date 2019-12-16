96b1000fdf438262d690a13fa85b47f230e26320
Allan O <a.onchuru@gmail.com>
2019-12-16 17:46:26 +0300

    :recycle: Refactor to make use of Util method

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/CoreBaseAncMedicalHistoryInteractor.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/CoreBaseAncMedicalHistoryInteractor.java
@@ -22,32 +22,12 @@ public class CoreBaseAncMedicalHistoryInteractor extends BaseAncMedicalHistoryIn
             final List<Visit> all_visits = new ArrayList<>(visits);
 
             for(Visit visit : visits){
-                List<Visit> child_visits = getChildVisits(visit.getVisitId());
+                List<Visit> child_visits = VisitUtils.getChildVisits(visit.getVisitId());
                 all_visits.addAll(child_visits);
             }
             appExecutors.mainThread().execute(() -> callBack.onDataFetched(all_visits));
         };
 
         appExecutors.diskIO().execute(runnable);
-    }
-
-    private List<Visit> getChildVisits(String parentVisitID){
-        List<Visit> res = new ArrayList<>();
-
-        List<Visit> visit_kids = AncLibrary.getInstance().visitRepository().getChildEvents(parentVisitID);
-
-        if (visit_kids != null && !visit_kids.isEmpty()) {
-            int x = 0;
-            while (x < visit_kids.size()) {
-                Visit v = visit_kids.get(x);
-                List<VisitDetail> visitDetails = AncLibrary.getInstance().visitDetailsRepository().getVisits(v.getVisitId());
-                visit_kids.get(x).setVisitDetails(VisitUtils.getVisitGroups(visitDetails));
-                x++;
-            }
-
-            res.addAll(visit_kids);
-        }
-
-        return res;
     }
 }
