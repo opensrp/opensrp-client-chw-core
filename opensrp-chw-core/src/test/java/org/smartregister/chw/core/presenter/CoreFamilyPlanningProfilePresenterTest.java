34e58b2eaee0d8193141fb489ed6c733cce6159f
Allan O <a.onchuru@gmail.com>
2020-02-08 17:58:46 +0300

    :white_check_mark: Add test for start fp referrals starts referral form

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/presenter/CoreFamilyPlanningProfilePresenterTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/presenter/CoreFamilyPlanningProfilePresenterTest.java
@@ -1,13 +1,17 @@
 package org.smartregister.chw.core.presenter;
 
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
 import org.smartregister.chw.core.contract.CoreFamilyPlanningMemberProfileContract;
 import org.smartregister.chw.fp.contract.BaseFpProfileContract;
 import org.smartregister.chw.fp.domain.FpMemberObject;
+import org.smartregister.util.FormUtils;
 
 public class CoreFamilyPlanningProfilePresenterTest {
     @Mock
@@ -19,9 +23,14 @@ public class CoreFamilyPlanningProfilePresenterTest {
     @Mock
     private BaseFpProfileContract.Interactor interactor;
 
+    @Mock
+    private JSONObject jsonForm;
 
     @Mock
     private FpMemberObject fpMemberObject;
+
+    @Mock
+    private FormUtils formUtils;
 
     @Before
     public void setUp() {
@@ -35,5 +44,11 @@ public class CoreFamilyPlanningProfilePresenterTest {
         Assert.assertEquals(view, myView);
     }
 
+    @Test
+    public void startFamilyPlanningReferralStartsReferralForm() {
+        Whitebox.setInternalState(profilePresenter, "formUtils", formUtils);
+        profilePresenter.startFamilyPlanningReferral();
+        Mockito.verify(profilePresenter.getView()).startFormActivity(Mockito.any(), Mockito.any());
+    }
 
 }
