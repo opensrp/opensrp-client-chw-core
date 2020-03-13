00dcd263a1adfe63241b5dc86cc8199d8fff7f85
Allan O <a.onchuru@gmail.com>
2020-03-13 08:34:06 +0300

    :recycle: Fix Codacy Reported Issue

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/utils/CoreJsonFormUtilsTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/utils/CoreJsonFormUtilsTest.java
@@ -71,126 +71,14 @@ public class CoreJsonFormUtilsTest extends BaseUnitTest {
     }
 
     private String getRemoveMemberJsonString(String encounterType, String baseEntityId) {
-        return "{\n" +
-                "    \"count\": \"1\",\n" +
-                "    \"encounter_type\": \"" + encounterType + "\",\n" +
-                "    \"entity_id\": \"" + baseEntityId + "\",\n" +
-                "    \"relational_id\": \"\",\n" +
-                "    \"metadata\": {},\n" +
-                "    \"step1\": {\n" +
-                "        \"title\": \"Remove Family Member\",\n" +
-                "        \"fields\": [\n" +
-                "            {\n" +
-                "                \"key\": \"details\",\n" +
-                "                \"openmrs_entity_parent\": \"\",\n" +
-                "                \"openmrs_entity\": \"concept\",\n" +
-                "                \"openmrs_entity_id\": \"\",\n" +
-                "                \"type\": \"label\",\n" +
-                "                \"text\": \"Melissa Yo Jiwanji, 25 Female\",\n" +
-                "                \"text_size\": \"25px\"\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"key\": \"remove_reason\",\n" +
-                "                \"openmrs_entity_parent\": \"\",\n" +
-                "                \"openmrs_entity\": \"concept\",\n" +
-                "                \"openmrs_entity_id\": \"160417AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\n" +
-                "                \"openmrs_data_type\": \"select one\",\n" +
-                "                \"type\": \"spinner\",\n" +
-                "                \"hint\": \"Reason for removal\",\n" +
-                "                \"v_required\": {\n" +
-                "                    \"value\": \"true\",\n" +
-                "                    \"err\": \"Select the reason for removing the family member's record\"\n" +
-                "                },\n" +
-                "                \"values\": [\n" +
-                "                    \"Death\",\n" +
-                "                    \"Moved away\",\n" +
-                "                    \"Other\"\n" +
-                "                ],\n" +
-                "                \"keys\": [\n" +
-                "                    \"Death\",\n" +
-                "                    \"Moved away\",\n" +
-                "                    \"Other\"\n" +
-                "                ],\n" +
-                "                \"openmrs_choice_ids\": {\n" +
-                "                    \"Died\": \"160034AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\n" +
-                "                    \"Moved away\": \"160415AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\n" +
-                "                    \"Other\": \"5622AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\"\n" +
-                "                },\n" +
-                "                \"is-rule-check\": true,\n" +
-                "                \"step\": \"step1\",\n" +
-                "                \"value\": \"Other\"\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"key\": \"dob\",\n" +
-                "                \"openmrs_entity_parent\": \"\",\n" +
-                "                \"openmrs_entity\": \"concept\",\n" +
-                "                \"openmrs_entity_id\": \"\",\n" +
-                "                \"type\": \"spacer\",\n" +
-                "                \"expanded\": false,\n" +
-                "                \"read_only\": \"true\",\n" +
-                "                \"hidden\": \"false\",\n" +
-                "                \"value\": \"01-04-1994\",\n" +
-                "                \"step\": \"step1\",\n" +
-                "                \"is-rule-check\": false\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"key\": \"date_moved\",\n" +
-                "                \"openmrs_entity_parent\": \"\",\n" +
-                "                \"openmrs_entity\": \"concept\",\n" +
-                "                \"openmrs_entity_id\": \"164133AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\n" +
-                "                \"openmrs_data_type\": \"text\",\n" +
-                "                \"type\": \"date_picker\",\n" +
-                "                \"label\": \"Date moved away\",\n" +
-                "                \"hint\": \"Date moved away\",\n" +
-                "                \"expanded\": false,\n" +
-                "                \"min_date\": \"today-9475d\",\n" +
-                "                \"max_date\": \"today\",\n" +
-                "                \"v_required\": {\n" +
-                "                    \"value\": \"true\",\n" +
-                "                    \"err\": \"Enter the date that the member moved away\"\n" +
-                "                },\n" +
-                "                \"is_visible\": false,\n" +
-                "                \"is-rule-check\": false\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"key\": \"date_died\",\n" +
-                "                \"openmrs_entity_parent\": \"\",\n" +
-                "                \"openmrs_entity\": \"concept\",\n" +
-                "                \"openmrs_entity_id\": \"1543AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\n" +
-                "                \"openmrs_data_type\": \"text\",\n" +
-                "                \"type\": \"date_picker\",\n" +
-                "                \"label\": \"Date of death\",\n" +
-                "                \"hint\": \"Date of death\",\n" +
-                "                \"expanded\": false,\n" +
-                "                \"min_date\": \"today-9475d\",\n" +
-                "                \"max_date\": \"today\",\n" +
-                "                \"v_required\": {\n" +
-                "                    \"value\": \"true\",\n" +
-                "                    \"err\": \"Enter the date of death\"\n" +
-                "                },\n" +
-                "                \"step\": \"step1\",\n" +
-                "                \"is-rule-check\": false,\n" +
-                "                \"is_visible\": false\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"key\": \"age_at_death\",\n" +
-                "                \"openmrs_entity_parent\": \"\",\n" +
-                "                \"openmrs_entity\": \"concept\",\n" +
-                "                \"openmrs_entity_id\": \"\",\n" +
-                "                \"label\": \"Age at death\",\n" +
-                "                \"hint\": \"Age at death\",\n" +
-                "                \"type\": \"edit_text\",\n" +
-                "                \"read_only\": \"true\",\n" +
-                "                \"is_visible\": false\n" +
-                "            }\n" +
-                "        ]\n" +
-                "    },\n" +
-                "    \"invisible_required_fields\": \"[date_died, date_moved]\",\n" +
-                "    \"details\": {\n" +
-                "        \"appVersionName\": \"1.7.23-SNAPSHOT\",\n" +
-                "        \"formVersion\": \"\"\n" +
-                "    }\n" +
-                "}";
+        return "{\"count\":\"1\",\"encounter_type\":\"" + encounterType + "\",\"entity_id\":\"" + baseEntityId + "\",\"relational_id\":\"\",\"metadata\":{},\"step1\":{\"title\":\"RemoveFamilyMember\",\"fields\":[{\"key\":\"details\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"concept\",\"openmrs_entity_id\":\"\",\"type\":" +
+                "\"label\",\"text\":\"MelissaYoJiwanji,25Female\",\"text_size\":\"25px\"},{\"key\":\"remove_reason\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"concept\",\"openmrs_entity_id\":\"160417AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\"openmrs_data_type\":\"selectone\",\"type\":\"spinner\",\"hint\":\"Reasonforremoval\",\"v_required\"" +
+                ":{\"value\":\"true\",\"err\":\"Selectthereasonforremovingthefamilymember'srecord\"},\"values\":[\"Death\",\"Movedaway\",\"Other\"],\"keys\":[\"Death\",\"Movedaway\",\"Other\"],\"openmrs_choice_ids\":{\"Died\":\"160034AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\"Movedaway\":\"160415AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\"Other\"" +
+                ":\"5622AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\"},\"is-rule-check\":true,\"step\":\"step1\",\"value\":\"Other\"},{\"key\":\"dob\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"concept\",\"openmrs_entity_id\":\"\",\"type\":\"spacer\",\"expanded\":false,\"read_only\":\"true\",\"hidden\":\"false\",\"value\":\"01-04-1994\",\"step\"" +
+                ":\"step1\",\"is-rule-check\":false},{\"key\":\"date_moved\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"concept\",\"openmrs_entity_id\":\"164133AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\"openmrs_data_type\":\"text\",\"type\":\"date_picker\",\"label\":\"Datemovedaway\",\"hint\":\"Datemovedaway\",\"expanded\":false,\"min_date\"" +
+                ":\"today-9475d\",\"max_date\":\"today\",\"v_required\":{\"value\":\"true\",\"err\":\"Enterthedatethatthemembermovedaway\"},\"is_visible\":false,\"is-rule-check\":false},{\"key\":\"date_died\",\"openmrs_entity_parent\":\"\",\"openmrs_entity\":\"concept\",\"openmrs_entity_id\":\"1543AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\"openmrs_data_type\"" +
+                ":\"text\",\"type\":\"date_picker\",\"label\":\"Dateofdeath\",\"hint\":\"Dateofdeath\",\"expanded\":false,\"min_date\":\"today-9475d\",\"max_date\":\"today\",\"v_required\":{\"value\":\"true\",\"err\":\"Enterthedateofdeath\"},\"step\":\"step1\",\"is-rule-check\":false,\"is_visible\":false},{\"key\":\"age_at_death\",\"openmrs_entity_parent\"" +
+                ":\"\",\"openmrs_entity\":\"concept\",\"openmrs_entity_id\":\"\",\"label\":\"Ageatdeath\",\"hint\":\"Ageatdeath\",\"type\":\"edit_text\",\"read_only\":\"true\",\"is_visible\":false}]},\"invisible_required_fields\":\"[date_died,date_moved]\",\"details\":{\"appVersionName\":\"1.7.23-SNAPSHOT\",\"formVersion\":\"\"}}";
     }
 
 }
