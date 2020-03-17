68fbf3a2c3bf2852f18f620ceddfb8237e8dc7ba
paulinembabu <pauline.mbabu@gmail.com>
2020-03-17 11:01:23 +0300

    Adds tests

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/BaseRobolectricTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/BaseRobolectricTest.java
@@ -1,4 +1,19 @@
 package org.smartregister.chw.core;
 
-public class BaseRobolectricTest {
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.smartregister.chw.core.application.TestApplication;
+import org.smartregister.chw.core.shadows.ShadowAssetHandler;
+import org.smartregister.chw.core.shadows.ShadowBaseJob;
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = {27}, shadows = {ShadowBaseJob.class, ShadowAssetHandler.class}, application = TestApplication.class)
+public abstract class BaseRobolectricTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
 }
--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/shadows/ShadowAssetHandler.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/shadows/ShadowAssetHandler.java
@@ -1,4 +1,22 @@
 package org.smartregister.chw.core.shadows;
 
+import android.content.Context;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+import org.smartregister.util.AssetHandler;
+
+@Implements(AssetHandler.class)
 public class ShadowAssetHandler {
+
+    @Implementation
+    public static String readFileFromAssetsFolder(String fileName, Context context) {
+       /* if ("special_vaccines.json".equals(fileName)) {
+            return VaccineDataUtils.SPECIAL_VACCINES_JSON;
+        } else if ("vaccines.json".equals(fileName)) {
+            return VaccineDataUtils.VACCINES_JSON;
+        }*/
+
+        return "";
+    }
 }
