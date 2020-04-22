ccf8632e8851a6e4381ad29022ed0cc1d1028c82
Manu Tarus <manutarus@gmail.com>
2020-04-22 09:07:48 +0300

    Add codacy suggested changes

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreFamilyProfileActivityTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreFamilyProfileActivityTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.smartregister.chw.core.BaseUnitTest;
@@ -16,7 +15,7 @@ public class CoreFamilyProfileActivityTest extends BaseUnitTest {
     private CoreFamilyProfileActivity controller;
     private String baseID = JsonFormUtils.generateRandomUUIDString();
 
-    CommonPersonObject commonPersonObject;
+    private CommonPersonObject commonPersonObject;
 
     @Before
     public void setUp() {
