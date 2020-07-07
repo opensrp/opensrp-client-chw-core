fa544e74b66d0dc3579b27ea41891d6bb27952ed
paulinembabu <pauline.mbabu@gmail.com>
2020-07-07 14:22:42 +0300

    Add test

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreStockInventoryItemDetailsReportActivityTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreStockInventoryItemDetailsReportActivityTest.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.core.activity;
 
+import android.content.Intent;
 import android.view.Menu;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,37 +9,35 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.Context;
 import org.smartregister.CoreLibrary;
 import org.smartregister.chw.core.BaseUnitTest;
 import org.smartregister.chw.core.adapter.CoreStockUsageItemDetailsAdapter;
-import org.smartregister.chw.core.model.StockUsageItemDetailsModel;
+import org.smartregister.chw.core.utils.CoreConstants;
 
-import java.util.List;
 
-import static org.junit.Assert.*;
-
+@RunWith(RobolectricTestRunner.class)
 public class CoreStockInventoryItemDetailsReportActivityTest extends BaseUnitTest {
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
     @Mock
-    private Menu menu;
-    @Mock
     RecyclerView recyclerView;
+    @Mock
+    private Menu menu;
     private CoreStockInventoryItemDetailsReportActivity activity;
-    private List<StockUsageItemDetailsModel> stockUsageItemDetailsModelList;
-    private String stockName;
-    private String stockMonth;
-    private String stockYear;
-    private String stockUsage;
-    private String providerName;
+
+    @Mock
+    private CoreStockUsageItemDetailsAdapter coreStockUsageItemDetailsAdapter;
 
     @Before
     public void setUp() {
@@ -47,13 +46,6 @@ public class CoreStockInventoryItemDetailsReportActivityTest extends BaseUnitTes
         MockitoAnnotations.initMocks(this);
         Context context = Context.getInstance();
         CoreLibrary.init(context);
-        //SetUp
-        stockName = "Male Condoms";
-        stockMonth = "12";
-        stockUsage = "20";
-        stockYear = "2019";
-        providerName = "All-CHWs";
-
 
         //Auto login by default
         String password = "pwd";
@@ -61,20 +53,22 @@ public class CoreStockInventoryItemDetailsReportActivityTest extends BaseUnitTes
         context.configuration().getDrishtiApplication().setPassword(password);
         context.session().setPassword(password);
 
-
         controller = Robolectric.buildActivity(CoreStockInventoryItemDetailsReportActivity.class).create().start();
         activity = controller.get();
     }
 
     @Test
-    public void  testEvaluateStockName(){
-        Assert.assertEquals("Male condom", activity.evaluateStockName(stockName));
-    }
-
-    @Test
-    public void tedtOnCreateOptionsMenu() {
+    public void testOnCreateOptionsMenu() {
         Assert.assertFalse(activity.onCreateOptionsMenu(menu));
 
+    }
+
+    private void getStockName(Intent intent) {
+        String stockItemName = "Male Condoms";
+        activity = Mockito.spy(activity);
+        intent.putExtra(CoreConstants.HfStockUsageUtil.STOCK_NAME, stockItemName);
+        activity.setIntent(intent);
+       // Mockito.doReturn(stockItemName).when(activity.getIntent()).getStringExtra(CoreConstants.HfStockUsageUtil.STOCK_NAME);
     }
 
     @Test
@@ -84,9 +78,9 @@ public class CoreStockInventoryItemDetailsReportActivityTest extends BaseUnitTes
         Assert.assertNotNull(ReflectionHelpers.getField(activity, "toolBarTextView"));
         Assert.assertNotNull(ReflectionHelpers.getField(activity, "appBarLayout"));
 
+        Intent intent = activity.getIntent();
+        getStockName(intent);
 
-        stockUsageItemDetailsModelList.add(new StockUsageItemDetailsModel(stockMonth, stockYear, stockUsage));
-        CoreStockUsageItemDetailsAdapter coreStockUsageItemDetailsAdapter = new CoreStockUsageItemDetailsAdapter(activity.stockUsageItemDetailsModelList(stockName, providerName));
         recyclerView.setAdapter(coreStockUsageItemDetailsAdapter);
     }
 
