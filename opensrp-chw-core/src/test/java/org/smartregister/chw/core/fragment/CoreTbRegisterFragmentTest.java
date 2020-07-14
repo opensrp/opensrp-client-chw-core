0c255cadba072ec1f25274adeabc7f2c37fb5d3d
cozej4 <ilakozejumanne@gmail.com>
2020-07-14 16:41:16 +0300

    Added more tests

---
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/fragment/CoreTbRegisterFragmentTest.java
@@ -0,0 +1,112 @@
+package org.smartregister.chw.core.fragment;
+
+import android.view.View;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.smartregister.chw.core.BaseUnitTest;
+
+import java.util.Set;
+
+public class CoreTbRegisterFragmentTest extends BaseUnitTest {
+
+    @Mock
+    private CoreTbRegisterFragment coreTbRegisterFragment;
+
+    @Mock
+    private View view;
+
+    @Mock
+    private Set<org.smartregister.configurableviews.model.View> visibleColumns;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+   
+
+    @Test
+    public void InitializeAdapter() {
+        Mockito.doNothing().when(coreTbRegisterFragment).initializeAdapter(visibleColumns);
+        coreTbRegisterFragment.initializeAdapter(visibleColumns);
+
+        ArgumentCaptor<Set> captor = ArgumentCaptor.forClass(Set.class);
+        Mockito.verify(coreTbRegisterFragment, Mockito.times(1)).initializeAdapter(captor.capture());
+        Assert.assertEquals(captor.getValue(), visibleColumns);
+    }
+
+    @Test
+    public void SetupViews() {
+        Mockito.doNothing().when(coreTbRegisterFragment).setupViews(view);
+        coreTbRegisterFragment.setupViews(view);
+
+        ArgumentCaptor<View> captor = ArgumentCaptor.forClass(View.class);
+        Mockito.verify(coreTbRegisterFragment, Mockito.times(1)).setupViews(captor.capture());
+        Assert.assertEquals(captor.getValue(), view);
+    }
+    
+    @Test
+    public void OnViewClicked() {
+        Mockito.doNothing().when(coreTbRegisterFragment).onViewClicked(view);
+        coreTbRegisterFragment.onViewClicked(view);
+
+        ArgumentCaptor<View> captor = ArgumentCaptor.forClass(View.class);
+        Mockito.verify(coreTbRegisterFragment, Mockito.times(1)).onViewClicked(captor.capture());
+        Assert.assertEquals(captor.getValue(), view);
+    }
+
+    @Test
+    public void DueFilters() {
+        Mockito.doNothing().when(coreTbRegisterFragment).dueFilter(view);
+        coreTbRegisterFragment.dueFilter(view);
+
+        ArgumentCaptor<View> captor = ArgumentCaptor.forClass(View.class);
+        Mockito.verify(coreTbRegisterFragment, Mockito.times(1)).dueFilter(captor.capture());
+        Assert.assertEquals(captor.getValue(), view);
+    }
+
+    @Test
+    public void ToggleFilterSelection() {
+        Mockito.doNothing().when(coreTbRegisterFragment).toggleFilterSelection(view);
+        coreTbRegisterFragment.toggleFilterSelection(view);
+
+        ArgumentCaptor<View> captor = ArgumentCaptor.forClass(View.class);
+        Mockito.verify(coreTbRegisterFragment, Mockito.times(1)).toggleFilterSelection(captor.capture());
+        Assert.assertEquals(captor.getValue(), view);
+    }
+    
+    @Test
+    public void FilterDue() {
+        String filterString = "filterString";
+        String joinTableString = "joinTableString";
+        String mainConditionString = "mainConditionString";
+        Mockito.doNothing().when(coreTbRegisterFragment).filterDue(filterString, joinTableString, mainConditionString);
+        coreTbRegisterFragment.filterDue(filterString, joinTableString, mainConditionString);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> captor1 = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> captor2 = ArgumentCaptor.forClass(String.class);
+        Mockito.verify(coreTbRegisterFragment, Mockito.times(1)).filterDue(captor.capture(), captor1.capture(), captor2.capture());
+        Assert.assertEquals(captor.getValue(), filterString);
+        Assert.assertEquals(captor1.getValue(), joinTableString);
+        Assert.assertEquals(captor2.getValue(), mainConditionString);
+    }
+    
+    @Test
+    public void NormalFilter() {
+        Mockito.doNothing().when(coreTbRegisterFragment).normalFilter(view);
+        coreTbRegisterFragment.normalFilter(view);
+
+        ArgumentCaptor<View> captor = ArgumentCaptor.forClass(View.class);
+        Mockito.verify(coreTbRegisterFragment, Mockito.times(1)).normalFilter(captor.capture());
+        Assert.assertEquals(captor.getValue(), view);
+    }
+
+
+}
