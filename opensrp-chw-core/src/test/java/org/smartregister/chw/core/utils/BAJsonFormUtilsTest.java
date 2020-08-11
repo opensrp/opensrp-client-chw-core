995b2a7de99ba9947bf7b89fe25295738b31ba92
Allan O <a.onchuru@gmail.com>
2020-08-11 11:34:26 +0300

    :white_check_mark: Add BAJsonFormUtils test class

---
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/utils/BAJsonFormUtilsTest.java
@@ -0,0 +1,20 @@
+package org.smartregister.chw.core.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.robolectric.annotation.Config;
+import org.smartregister.chw.core.BaseUnitTest;
+
+@Config(shadows = {})
+public class BAJsonFormUtilsTest extends BaseUnitTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void getAutoJsonEditMemberFormStringReturnsCorrectJSONObject() {
+
+    }
+}
