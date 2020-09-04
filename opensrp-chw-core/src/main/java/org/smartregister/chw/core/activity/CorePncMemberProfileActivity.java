844975158cf4df3d92eb998016be510eccc9d7b9
paulinembabu <pauline.mbabu@gmail.com>
2020-09-04 16:01:18 +0300

    BA support Fixes

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CorePncMemberProfileActivity.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/CorePncMemberProfileActivity.java
@@ -28,6 +28,7 @@ import org.smartregister.chw.core.rule.MalariaFollowUpRule;
 import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.core.utils.CoreJsonFormUtils;
 import org.smartregister.chw.core.utils.MalariaVisitUtil;
+import org.smartregister.chw.core.utils.UpdateMalariaFollowUpStatusTaskUtil;
 import org.smartregister.chw.malaria.dao.MalariaDao;
 import org.smartregister.chw.pnc.activity.BasePncMemberProfileActivity;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
@@ -145,18 +146,12 @@ public abstract class CorePncMemberProfileActivity extends BasePncMemberProfileA
             menu.add(0, R.id.action_pnc_remove_baby, 700 + i, getString(R.string.remove_child_form_title, childModels.get(i).getFirstName()));
             menuItemRemoveNames.put(getString(R.string.remove_child_form_title, childModels.get(i).getFirstName()), childModels.get(i).getBaseEntityId());
             if (MalariaDao.isRegisteredForMalaria(baseEntityID)) {
-                Utils.startAsyncTask(new UpdateMalariaFollowUpStatusTask(menu, baseEntityID), null);
+                Utils.startAsyncTask(new UpdateMalariaFollowUpStatusTaskUtil(menu, baseEntityID), null);
             } else {
                 menu.findItem(R.id.action_malaria_registration).setVisible(true);
             }
         }
         return true;
-    }
-
-    private void setMalariaFollowUpMenuItems(String followStatus, Menu menu) {
-        if (CoreConstants.VISIT_STATE.DUE.equalsIgnoreCase(followStatus) || CoreConstants.VISIT_STATE.OVERDUE.equalsIgnoreCase(followStatus)) {
-            menu.findItem(R.id.action_malaria_followup_visit).setVisible(true);
-        }
     }
 
     private boolean getEditMenuItem(MenuItem item) {
@@ -265,31 +260,5 @@ public abstract class CorePncMemberProfileActivity extends BasePncMemberProfileA
     protected abstract void startHfMalariaFollowupForm();
 
     protected abstract void getRemoveBabyMenuItem(MenuItem item);
-
-    private class UpdateMalariaFollowUpStatusTask extends AsyncTask<Void, Void, Void> {
-        private final Menu menu;
-        private final String baseEntityId;
-        private MalariaFollowUpRule malariaFollowUpRule;
-
-        private UpdateMalariaFollowUpStatusTask(Menu menu, String baseEntityId) {
-            this.menu = menu;
-            this.baseEntityId = baseEntityId;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Date malariaTestDate = MalariaDao.getMalariaTestDate(baseEntityId);
-            Date followUpDate = MalariaDao.getMalariaFollowUpVisitDate(baseEntityId);
-            malariaFollowUpRule = MalariaVisitUtil.getMalariaStatus(malariaTestDate, followUpDate);
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void param) {
-            if (malariaFollowUpRule != null) {
-                setMalariaFollowUpMenuItems(malariaFollowUpRule.getButtonStatus(), menu);
-            }
-        }
-    }
 
 }
