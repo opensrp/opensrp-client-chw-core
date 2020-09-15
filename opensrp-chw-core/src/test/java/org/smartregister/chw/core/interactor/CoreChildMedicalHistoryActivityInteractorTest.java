7cd4db8b9a288ce38c11928241655e876b87d08d
Allan O <a.onchuru@gmail.com>
2020-09-15 21:12:47 +0300

    :white_check_mark: Add VisitDaoShadowHelper

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/interactor/CoreChildMedicalHistoryActivityInteractorTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/interactor/CoreChildMedicalHistoryActivityInteractorTest.java
@@ -1,30 +1,34 @@
 package org.smartregister.chw.core.interactor;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.smartregister.chw.anc.domain.Visit;
+import org.smartregister.chw.core.BaseUnitTest;
 import org.smartregister.chw.core.application.TestApplication;
 import org.smartregister.chw.core.shadows.ContextShadow;
 
+import java.util.List;
+
 @RunWith(RobolectricTestRunner.class)
 @Config(application = TestApplication.class, shadows = {ContextShadow.class})
-public class CoreChildMedicalHistoryActivityInteractorTest {
+public class CoreChildMedicalHistoryActivityInteractorTest extends BaseUnitTest {
+
+    private CoreChildMedicalHistoryActivityInteractor interactor;
 
     @Before
     public void setUp() {
-
+        interactor = Mockito.mock(CoreChildMedicalHistoryActivityInteractor.class, Mockito.CALLS_REAL_METHODS);
     }
 
     @Test
-    public void canGetServiceRecords() {
-
-    }
-
-    @Test
-    public void canGetVisits() {
-
+    public void getVisitsReturnsCorrectList() {
+        List<Visit> visits = interactor.getVisits("memberID");
+        Assert.assertNotNull(visits);
     }
 
 }
