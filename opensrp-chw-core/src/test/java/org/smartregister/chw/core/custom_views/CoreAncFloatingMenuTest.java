242a7e05f4c4d802f3e6ec9f8647acae1ea6d358
paulinembabu <pauline.mbabu@gmail.com>
2020-09-22 17:42:23 +0300

    Add test

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/custom_views/CoreAncFloatingMenuTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/custom_views/CoreAncFloatingMenuTest.java
@@ -1,6 +1,12 @@
 package org.smartregister.chw.core.custom_views;
 
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,8 +15,13 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.chw.core.BaseUnitTest;
 import org.smartregister.chw.core.listener.OnClickFloatingMenu;
+
+import static org.junit.Assert.assertEquals;
 
 public class CoreAncFloatingMenuTest extends BaseUnitTest {
 
@@ -23,36 +34,49 @@ public class CoreAncFloatingMenuTest extends BaseUnitTest {
     @Mock
     private OnClickFloatingMenu onClickFloatingMenu;
 
+    @Mock
+    private RelativeLayout relativeLayout;
+
+    @Mock
+    private LinearLayout linearLayout;
+
+    @Mock
+    private FloatingActionButton floatingActionButton;
+
+    private ActivityController<AppCompatActivity> activityController;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        coreAncFloatingMenu = Mockito.mock(CoreAncFloatingMenu.class, Mockito.CALLS_REAL_METHODS);
+
+        ReflectionHelpers.setField(coreAncFloatingMenu, "onClickFloatingMenu", onClickFloatingMenu);
+        ReflectionHelpers.setField(coreAncFloatingMenu, "callLayout", view);
+        ReflectionHelpers.setField(coreAncFloatingMenu, "activityMain", relativeLayout);
+        ReflectionHelpers.setField(coreAncFloatingMenu, "fab", floatingActionButton);
+        ReflectionHelpers.setField(coreAncFloatingMenu, "menuBar", linearLayout);
+        ReflectionHelpers.setField(coreAncFloatingMenu, "referLayout", view);
+        ReflectionHelpers.setField(coreAncFloatingMenu, "isFabMenuOpen", true);
+
+        activityController = Robolectric.buildActivity(AppCompatActivity.class).create().resume();
     }
 
     @Test
     public void whenSetupViewsAnswered() {
-        Mockito.doNothing().when(coreAncFloatingMenu).setFloatMenuClickListener(onClickFloatingMenu);
         coreAncFloatingMenu.setFloatMenuClickListener(onClickFloatingMenu);
 
         ArgumentCaptor<OnClickFloatingMenu> captor = ArgumentCaptor.forClass(OnClickFloatingMenu.class);
         Mockito.verify(coreAncFloatingMenu, Mockito.times(1)).setFloatMenuClickListener(captor.capture());
         Assert.assertEquals(captor.getValue(), onClickFloatingMenu);
     }
+
     @Test
     public void whenOnClickAnswered() {
-        Mockito.doNothing().when(coreAncFloatingMenu).onClick(view);
         coreAncFloatingMenu.onClick(view);
 
         ArgumentCaptor<View> captor = ArgumentCaptor.forClass(View.class);
         Mockito.verify(coreAncFloatingMenu, Mockito.times(1)).onClick(captor.capture());
         Assert.assertEquals(captor.getValue(), view);
-    }
-    @Test
-    public void whenRedrawAnswered() {
-        Mockito.doNothing().when(coreAncFloatingMenu).redraw(true);
-        coreAncFloatingMenu.redraw(true);
-        ArgumentCaptor<Boolean> captor = ArgumentCaptor.forClass(Boolean.class);
-        Mockito.verify(coreAncFloatingMenu, Mockito.times(1)).redraw(captor.capture());
-        Assert.assertEquals(captor.getValue(), true);
     }
 
     @Test
@@ -60,6 +84,20 @@ public class CoreAncFloatingMenuTest extends BaseUnitTest {
         Mockito.when(coreAncFloatingMenu.getCallLayout())
                 .thenReturn(view);
         Assert.assertEquals(view, coreAncFloatingMenu.getCallLayout());
+    }
+
+
+    @Test
+    public void testAnimateFABWhenMenuBarVisible() {
+        coreAncFloatingMenu.animateFAB();
+        assertEquals(View.VISIBLE, coreAncFloatingMenu.menuBar.getVisibility());
+    }
+
+    @Test
+    public void testAnimateFABWhenMenuBarNotVisible() {
+        ReflectionHelpers.setField(coreAncFloatingMenu, "isFabMenuOpen", false);
+        coreAncFloatingMenu.animateFAB();
+        assertEquals(View.VISIBLE, coreAncFloatingMenu.menuBar.getVisibility());
     }
 
 }
