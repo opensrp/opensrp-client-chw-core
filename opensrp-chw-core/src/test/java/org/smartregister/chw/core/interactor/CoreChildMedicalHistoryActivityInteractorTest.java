040f48c099f897c73912cda0ba9c95f215a0e006
Allan O <a.onchuru@gmail.com>
2020-09-22 15:05:19 +0300

    :white_check_mark: Fix test

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/interactor/CoreChildMedicalHistoryActivityInteractorTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/interactor/CoreChildMedicalHistoryActivityInteractorTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.smartregister.chw.anc.domain.Visit;
@@ -12,7 +13,6 @@ import org.smartregister.chw.core.BaseUnitTest;
 import org.smartregister.chw.core.application.TestApplication;
 import org.smartregister.chw.core.shadows.ContextShadow;
 import org.smartregister.chw.core.shadows.VisitDaoShadowHelper;
-
 import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
@@ -23,6 +23,7 @@ public class CoreChildMedicalHistoryActivityInteractorTest extends BaseUnitTest 
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         interactor = Mockito.mock(CoreChildMedicalHistoryActivityInteractor.class, Mockito.CALLS_REAL_METHODS);
     }
 
@@ -30,7 +31,9 @@ public class CoreChildMedicalHistoryActivityInteractorTest extends BaseUnitTest 
     public void getVisitsReturnsCorrectList() {
         List<Visit> visits = interactor.getVisits("memberID");
         Assert.assertEquals(1, visits.size());
+        Assert.assertEquals(1, visits.get(0).getVisitDetails().size());
         Assert.assertEquals("test-visit-id", visits.get(0).getVisitId());
+        Assert.assertEquals("Exclusive Breastfeeding", visits.get(0).getVisitType());
         Assert.assertEquals("test_details", visits.get(0).getVisitDetails().get("test_visit_key").get(0).getDetails());
     }
 
