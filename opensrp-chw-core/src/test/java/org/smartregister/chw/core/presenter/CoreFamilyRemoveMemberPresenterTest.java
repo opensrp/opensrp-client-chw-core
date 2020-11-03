b5955b68e7251bdfa66ddca6eed759d3e47db1d6
Allan O <a.onchuru@gmail.com>
2020-11-03 12:58:25 +0300

    :white_check_mark: Add CoreFamilyRemoveMemberPresenter tests

---
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/presenter/CoreFamilyRemoveMemberPresenterTest.java
@@ -0,0 +1,84 @@
+package org.smartregister.chw.core.presenter;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.util.ReflectionHelpers;
+import org.smartregister.chw.core.contract.FamilyRemoveMemberContract;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.family.util.DBConstants;
+import org.smartregister.opd.utils.OpdDbConstants;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+
+
+@PrepareForTest(CoreFamilyRemoveMemberPresenter.class)
+public class CoreFamilyRemoveMemberPresenterTest {
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+
+    @Mock
+    private FamilyRemoveMemberContract.View view;
+
+    @Mock
+    FamilyRemoveMemberContract.Interactor interactor;
+
+    @Mock
+    FamilyRemoveMemberContract.Model model;
+
+    private CoreFamilyRemoveMemberPresenter removeMemberPresenter;
+
+    private CommonPersonObjectClient commonPersonClient;
+
+    private HashMap<String, String> columnMaps;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        removeMemberPresenter = Mockito.mock(CoreFamilyRemoveMemberPresenter.class, Mockito.CALLS_REAL_METHODS);
+        ReflectionHelpers.setField(removeMemberPresenter, "viewReference", new WeakReference<>(view));
+        ReflectionHelpers.setField(removeMemberPresenter, "interactor", interactor);
+        ReflectionHelpers.setField(removeMemberPresenter, "model", model);
+        ReflectionHelpers.setField(removeMemberPresenter, "familyHead", "family-head");
+        ReflectionHelpers.setField(removeMemberPresenter, "primaryCaregiver", "primary-care-giver");
+        ReflectionHelpers.setField(removeMemberPresenter, "familyBaseEntityId", "family-entity-id");
+
+        commonPersonClient = new CommonPersonObjectClient("test-case-id", new HashMap<>(), "test");
+        columnMaps = new HashMap<>();
+
+        commonPersonClient.setColumnmaps(columnMaps);
+    }
+
+    @Test
+    public void removeMemberStartsRemoveMemberForm() throws Exception {
+        columnMaps.put(DBConstants.KEY.BASE_ENTITY_ID, "");
+        columnMaps.put(OpdDbConstants.KEY.REGISTER_TYPE, "");
+        CoreFamilyRemoveMemberPresenter presenterSpy = PowerMockito.spy(removeMemberPresenter);
+        PowerMockito.doNothing().when(presenterSpy, "startRemoveMemberForm", ArgumentMatchers.any(CommonPersonObjectClient.class));
+        presenterSpy.removeMember(commonPersonClient);
+        PowerMockito.verifyPrivate(presenterSpy).invoke("startRemoveMemberForm", ArgumentMatchers.any(CommonPersonObjectClient.class));
+    }
+
+
+    @Test
+    public void removeFamilyHeadInvokesProcessFamilyMember() {
+        columnMaps.put(DBConstants.KEY.BASE_ENTITY_ID, "family-head");
+        columnMaps.put(OpdDbConstants.KEY.REGISTER_TYPE, "");
+
+        removeMemberPresenter.removeMember(commonPersonClient);
+        Mockito.verify(interactor, Mockito.times(1)).processFamilyMember(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.any(CommonPersonObjectClient.class),
+                ArgumentMatchers.any(CoreFamilyRemoveMemberPresenter.class));
+    }
+
+}
