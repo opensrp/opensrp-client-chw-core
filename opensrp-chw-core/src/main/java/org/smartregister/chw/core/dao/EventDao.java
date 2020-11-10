e6883b596767d16958a0797ee7c934821d409ceb
rkodev <43806892+rkodev@users.noreply.github.com>
2020-11-10 21:00:15 +0300

    Remove deleted vaccines from history page

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/dao/EventDao.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/dao/EventDao.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.core.dao;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,7 +9,9 @@ import org.smartregister.clientandeventmodel.Event;
 import org.smartregister.dao.AbstractDao;
 import org.smartregister.sync.helper.ECSyncHelper;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import timber.log.Timber;
 
@@ -78,6 +81,15 @@ public class EventDao extends AbstractDao {
     }
 
     public static void deleteVaccineByFormSubmissionId(String submissionId) {
+        String sql0 = "delete from visit_details where visit_details_id in ( " +
+                " select vd.visit_details_id " +
+                " from visit_details vd  " +
+                " inner join visits v on v.visit_id = vd.visit_id " +
+                " inner join vaccines va on va.name = visit_key and va.base_entity_id = v.base_entity_id " +
+                " where va.formSubmissionId = '" + submissionId + "' " +
+                ")";
+        updateDB(sql0);
+
         String sql = "delete from vaccines where formSubmissionId = '" + submissionId + "'";
         updateDB(sql);
     }
