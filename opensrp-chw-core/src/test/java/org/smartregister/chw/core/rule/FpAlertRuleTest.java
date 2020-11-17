a2711cef9b61b9757b99849745ae1eb45433a328
Allan O <a.onchuru@gmail.com>
2020-11-17 12:37:40 +0300

    :white_check_mark: Add get visitID, check sterilization valid tests

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/rule/FpAlertRuleTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/rule/FpAlertRuleTest.java
@@ -1,6 +1,9 @@
 package org.smartregister.chw.core.rule;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.chw.core.BaseRobolectricTest;
 import org.smartregister.chw.fp.util.FamilyPlanningConstants;
 
@@ -16,10 +19,21 @@ public class FpAlertRuleTest extends BaseRobolectricTest {
         Date fpDate = new SimpleDateFormat("dd-MM-yyyy").parse("17-11-2020");
         Date lastVisitDate = new SimpleDateFormat("dd-MM-yyyy").parse("17-10-2020");
         fpAlertRule = new FpAlertRule(fpDate, lastVisitDate, 10, FamilyPlanningConstants.DBConstants.FP_COC);
+        ReflectionHelpers.setField(fpAlertRule, "visitID", "test-visit-id");
     }
 
 
+    @Test
+    public void canGetVisitId() {
+        Assert.assertEquals("test-visit-id", fpAlertRule.getVisitID());
+    }
 
 
+    @Test
+    public void canGetIfFemaleSterilizationFollowUpOneValid() {
+        ReflectionHelpers.setField(fpAlertRule, "fpDifference", 5);
+        Assert.assertFalse(fpAlertRule.isFemaleSterilizationFollowUpOneValid(10, 15, 20));
+        Assert.assertTrue(fpAlertRule.isFemaleSterilizationFollowUpOneValid(2, 6, 10));
+    }
 
 }
