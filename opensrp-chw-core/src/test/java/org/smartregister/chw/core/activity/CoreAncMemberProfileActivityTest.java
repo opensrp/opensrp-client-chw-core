6f8b9272a766625d41b438443a79e48253163d26
rkodev <43806892+rkodev@users.noreply.github.com>
2020-11-18 11:47:20 +0300

    Add tests

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreAncMemberProfileActivityTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreAncMemberProfileActivityTest.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.core.activity;
 
+import android.view.MenuItem;
 import android.view.View;
 
 import org.junit.Assert;
@@ -8,15 +9,25 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.util.ReflectionHelpers;
+import org.smartregister.Context;
+import org.smartregister.CoreLibrary;
 import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.chw.core.BaseUnitTest;
+import org.smartregister.chw.core.R;
+import org.smartregister.chw.core.activity.impl.CoreAncMemberProfileActivityImpl;
+import org.smartregister.chw.core.presenter.CoreAncMemberProfilePresenter;
+import org.smartregister.chw.core.utils.CoreConstants;
 
-public class CoreAncMemberProfileActivityTest {
+public class CoreAncMemberProfileActivityTest extends BaseUnitTest {
 
     private CoreAncMemberProfileActivity activity;
 
     private View viewFamilyRow = new View(RuntimeEnvironment.systemContext);
+    private ActivityController<CoreAncMemberProfileActivityImpl> controller;
 
     @Mock
     private MemberObject memberObject;
@@ -24,9 +35,21 @@ public class CoreAncMemberProfileActivityTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        activity = Mockito.mock(CoreAncMemberProfileActivity.class, Mockito.CALLS_REAL_METHODS);
-        String baseEntityId = "base-entity-id";
-        memberObject.setBaseEntityId(baseEntityId);
+
+        MockitoAnnotations.initMocks(this);
+
+        Context context = Context.getInstance();
+        CoreLibrary.init(context);
+
+        //Auto login by default
+        context.session().start(context.session().lengthInMilliseconds());
+
+        controller = Robolectric.buildActivity(CoreAncMemberProfileActivityImpl.class).create().start();
+        activity = controller.get();
+        memberObject = Mockito.mock(MemberObject.class);
+        memberObject.setBaseEntityId("some-base-entity-id");
+        memberObject.setFamilyName("Some Family Name");
+
         ReflectionHelpers.setField(activity, "memberObject", memberObject);
         ReflectionHelpers.setField(activity, "view_family_row", viewFamilyRow);
     }
@@ -38,7 +61,46 @@ public class CoreAncMemberProfileActivityTest {
     }
 
     @Test
+    public void testOnOptionsItemSelected() {
+        activity = Mockito.spy(activity);
+
+        MenuItem menuItem = Mockito.mock(MenuItem.class);
+        CoreConstants.JSON_FORM.setLocaleAndAssetManager(activity.getApplicationContext().getResources().getConfiguration().locale, activity.getApplicationContext().getAssets());
+
+        // back pressed
+        Mockito.doReturn(android.R.id.home).when(menuItem).getItemId();
+        Mockito.doNothing().when(activity).onBackPressed();
+
+        activity.onOptionsItemSelected(menuItem);
+        Mockito.verify(activity).onBackPressed();
+
+        // start form
+        Mockito.doReturn(R.id.action_anc_member_registration).when(menuItem).getItemId();
+        Mockito.doNothing().when(activity).startFormForEdit(Mockito.any(), Mockito.any());
+
+        activity.onOptionsItemSelected(menuItem);
+        Mockito.verify(activity).startFormForEdit(Mockito.eq(R.string.edit_member_form_title), Mockito.any());
+
+
+        // start form
+        Mockito.doReturn(R.id.action_anc_registration).when(menuItem).getItemId();
+        Mockito.doNothing().when(activity).startFormForEdit(Mockito.any(), Mockito.any());
+
+        activity.onOptionsItemSelected(menuItem);
+        Mockito.verify(activity).startFormForEdit(Mockito.eq(R.string.edit_anc_registration_form_title), Mockito.any());
+
+        // start form
+        Mockito.doReturn(R.id.anc_danger_signs_outcome).when(menuItem).getItemId();
+        CoreAncMemberProfilePresenter presenter = Mockito.mock(CoreAncMemberProfilePresenter.class);
+        ReflectionHelpers.setField(activity, "presenter", presenter);
+
+        activity.onOptionsItemSelected(menuItem);
+        Mockito.verify(presenter).startAncDangerSignsOutcomeForm(memberObject);
+    }
+
+    @Test
     public void openFamilyLocationStartsAncMemberMapActivity() {
+        activity = Mockito.spy(activity);
         activity.openFamilyLocation();
         Mockito.verify(activity).startActivity(Mockito.any());
     }
