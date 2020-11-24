d6e5b8a681153feed9dd8b8ae1869becfebbc5e4
Allan O <a.onchuru@gmail.com>
2020-11-24 13:07:22 +0300

    :white_check_mark: Add button status tests

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/rule/FpAlertRuleTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/rule/FpAlertRuleTest.java
@@ -1,24 +1,36 @@
 package org.smartregister.chw.core.rule;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.chw.core.BaseRobolectricTest;
+import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.fp.util.FamilyPlanningConstants;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FpAlertRuleTest extends BaseRobolectricTest {
 
     private FpAlertRule fpAlertRule;
+    private String dateTimeFormat = "dd-MM-yyyy HH:mm:ss";
+    DateTime lastVisitDate;
+    DateTime dueDate;
+    DateTime expiryDate;
+    DateTime overDueDate;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws ParseException {
         Date fpDate = new SimpleDateFormat("dd-MM-yyyy").parse("17-11-2020");
-        Date lastVisitDate = new SimpleDateFormat("dd-MM-yyyy").parse("17-10-2020");
-        fpAlertRule = new FpAlertRule(fpDate, lastVisitDate, 10, FamilyPlanningConstants.DBConstants.FP_COC);
+        Date lastVisit = new SimpleDateFormat("dd-MM-yyyy").parse("17-10-2020");
+        fpAlertRule = new FpAlertRule(fpDate, lastVisit, 10, FamilyPlanningConstants.DBConstants.FP_COC);
         ReflectionHelpers.setField(fpAlertRule, "visitID", "test-visit-id");
     }
 
@@ -35,5 +47,58 @@ public class FpAlertRuleTest extends BaseRobolectricTest {
         Assert.assertFalse(fpAlertRule.isFemaleSterilizationFollowUpOneValid(10, 15, 20));
         Assert.assertTrue(fpAlertRule.isFemaleSterilizationFollowUpOneValid(2, 6, 10));
     }
+
+    @Test
+    public void buttonStatusDoneIfVisitBeforeExpiry() {
+        lastVisitDate = LocalDate.now().toDateTime(LocalTime.MIDNIGHT);
+        dueDate = LocalDate.now().minusDays(5).toDateTime(LocalTime.MIDNIGHT);
+        expiryDate = LocalDate.now().plusDays(10).toDateTime(LocalTime.MIDNIGHT);
+
+        ReflectionHelpers.setField(fpAlertRule, "dueDate", dueDate);
+        ReflectionHelpers.setField(fpAlertRule, "expiryDate", expiryDate);
+        ReflectionHelpers.setField(fpAlertRule, "lastVisitDate", lastVisitDate);
+        Assert.assertEquals(CoreConstants.VISIT_STATE.VISIT_DONE, fpAlertRule.getButtonStatus());
+    }
+
+    @Test
+    public void buttonStatusDueIfVisitBeforeOverdue() {
+        lastVisitDate = LocalDate.now().minusDays(10).toDateTime(LocalTime.MIDNIGHT);
+        dueDate = LocalDate.now().minusDays(5).toDateTime(LocalTime.MIDNIGHT);
+        overDueDate = LocalDate.now().plusDays(10).toDateTime(LocalTime.MIDNIGHT);
+
+        ReflectionHelpers.setField(fpAlertRule, "dueDate", dueDate);
+        ReflectionHelpers.setField(fpAlertRule, "overDueDate", overDueDate);
+        ReflectionHelpers.setField(fpAlertRule, "lastVisitDate", lastVisitDate);
+        Assert.assertEquals(CoreConstants.VISIT_STATE.DUE, fpAlertRule.getButtonStatus());
+    }
+
+
+    @Test
+    public void buttonStatusOverDueIfVisitAfterOverdue() {
+        lastVisitDate = LocalDate.now().minusDays(10).toDateTime(LocalTime.MIDNIGHT);
+        dueDate = LocalDate.now().minusDays(5).toDateTime(LocalTime.MIDNIGHT);
+        overDueDate = LocalDate.now().minusDays(1).toDateTime(LocalTime.MIDNIGHT);
+        expiryDate = LocalDate.now().plusDays(10).toDateTime(LocalTime.MIDNIGHT);
+
+        ReflectionHelpers.setField(fpAlertRule, "dueDate", dueDate);
+        ReflectionHelpers.setField(fpAlertRule, "expiryDate", expiryDate);
+        ReflectionHelpers.setField(fpAlertRule, "overDueDate", overDueDate);
+        ReflectionHelpers.setField(fpAlertRule, "lastVisitDate", lastVisitDate);
+        Assert.assertEquals(CoreConstants.VISIT_STATE.OVERDUE, fpAlertRule.getButtonStatus());
+    }
+
+
+    @Test
+    public void buttonStatusNotDueIfDateBeforeDueAndExpiry() {
+        lastVisitDate = LocalDate.now().toDateTime(LocalTime.MIDNIGHT);
+        dueDate = LocalDate.now().plusDays(10).toDateTime(LocalTime.MIDNIGHT);
+        expiryDate = LocalDate.now().plusDays(20).toDateTime(LocalTime.MIDNIGHT);
+
+        ReflectionHelpers.setField(fpAlertRule, "dueDate", dueDate);
+        ReflectionHelpers.setField(fpAlertRule, "expiryDate", expiryDate);
+        ReflectionHelpers.setField(fpAlertRule, "lastVisitDate", lastVisitDate);
+        Assert.assertEquals(CoreConstants.VISIT_STATE.NOT_DUE_YET, fpAlertRule.getButtonStatus());
+    }
+
 
 }
