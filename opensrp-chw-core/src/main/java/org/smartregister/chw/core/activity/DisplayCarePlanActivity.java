79406bb50753eb6138a28bbbc160ed37dec41dcb
zzainulabidin <62241922+zzainulabidin@users.noreply.github.com>
2020-12-23 14:19:55 +0500

    Diaply thinkmd child care plan

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/DisplayCarePlanActivity.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/activity/DisplayCarePlanActivity.java
@@ -10,28 +10,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.appbar.AppBarLayout;
 
 import org.smartregister.chw.core.R;
+import org.smartregister.chw.core.server.FileHTTPServer;
+
+import java.io.IOException;
+
+import timber.log.Timber;
 
 import static org.smartregister.chw.core.utils.CoreConstants.INTENT_KEY.CONTENT_TO_DISPLAY;
 
-public class WebViewActivity extends AppCompatActivity {
+public class DisplayCarePlanActivity extends AppCompatActivity {
+
     protected AppBarLayout appBarLayout;
+    private FileHTTPServer fileHTTPServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_view);
+        setContentView(R.layout.activity_display_care_plan);
 
         initObjects();
         if (getIntent().hasExtra(CONTENT_TO_DISPLAY) &&
                 getIntent().getStringExtra(CONTENT_TO_DISPLAY) != null) {
+            
             String contentToDisplay = getIntent().getStringExtra(CONTENT_TO_DISPLAY);
 
             contentToDisplay = contentToDisplay.replace("&lt;", "<")
                     .replace("&gt;", ">");
 
+            try {
+                fileHTTPServer = new FileHTTPServer(this, contentToDisplay);
+                fileHTTPServer.start();
+            } catch (IOException e) {
+                Timber.e(e);
+            }
+
             WebView webView = findViewById(R.id.web_view);
             webView.getSettings().setJavaScriptEnabled(true);
-            webView.loadDataWithBaseURL(null, contentToDisplay, "text/html", "utf-8", null);
+            webView.loadUrl("http://localhost:8085/index.html");
         } else {
             showNoContentAlertDialog();
         }
@@ -40,7 +55,7 @@ public class WebViewActivity extends AppCompatActivity {
 
     private void initObjects() {
         ImageView ivBack = findViewById(R.id.iv_back);
-        ivBack.setOnClickListener(view -> WebViewActivity.this.finish());
+        ivBack.setOnClickListener(view -> DisplayCarePlanActivity.this.finish());
     }
 
     private void showNoContentAlertDialog() {
@@ -50,7 +65,14 @@ public class WebViewActivity extends AppCompatActivity {
                 (dialog, which) -> {
                     dialog.dismiss();
                     this.finish();
+                    DisplayCarePlanActivity.this.finish();
                 });
         alertDialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        fileHTTPServer.destroy();
     }
 }
