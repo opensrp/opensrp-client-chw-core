3691ef2e031fcf7fb73f07ab9eb4be6c3872e5ca
paulinembabu <pauline.mbabu@gmail.com>
2021-01-19 15:27:40 +0300

    Add more test

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/fragment/FamilyCallDialogFragmentTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/fragment/FamilyCallDialogFragmentTest.java
@@ -1,10 +1,10 @@
 package org.smartregister.chw.core.fragment;
 
+import android.util.Pair;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.Robolectric;
-import org.robolectric.android.controller.ActivityController;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.Context;
 import org.smartregister.CoreLibrary;
@@ -26,6 +25,7 @@ import org.smartregister.receiver.SyncStatusBroadcastReceiver;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -47,16 +47,19 @@ public class FamilyCallDialogFragmentTest extends BaseUnitTest {
     @Mock
     private LinearLayout linearLayout;
 
-
-    private FragmentActivity activity;
-
-    private ActivityController controller;
-
-
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        fragment = Mockito.mock(FamilyCallDialogFragment.class, Mockito.CALLS_REAL_METHODS);
+        AppCompatActivity activity = Robolectric.buildActivity(AppCompatActivity.class).create().start().get();
+
+        fragment = new FamilyCallDialogFragment();
+
+        CoreLibrary.init(context);
+        when(context.commonrepository(anyString())).thenReturn(commonRepository);
+        Context.bindtypes = new ArrayList<>();
+        SyncStatusBroadcastReceiver.init(activity);
+        activity.getFragmentManager().beginTransaction().add(fragment, "Presenter").commit();
+
         ReflectionHelpers.setField(fragment, "tvFamilyHeadTitle", textView);
         ReflectionHelpers.setField(fragment, "tvFamilyHeadName", textView);
         ReflectionHelpers.setField(fragment, "tvFamilyHeadPhone", textView);
@@ -68,12 +71,6 @@ public class FamilyCallDialogFragmentTest extends BaseUnitTest {
         ReflectionHelpers.setField(fragment, "llCareGiver", linearLayout);
         ReflectionHelpers.setField(fragment, "familyBaseEntityId", "12345");
 
-        CoreLibrary.init(context);
-        when(context.commonrepository(anyString())).thenReturn(commonRepository);
-        controller = Robolectric.buildActivity(AppCompatActivity.class).create().resume();
-        activity = (FragmentActivity) controller.get();
-        Context.bindtypes = new ArrayList<>();
-        SyncStatusBroadcastReceiver.init(activity);
     }
 
     @Test
@@ -83,6 +80,20 @@ public class FamilyCallDialogFragmentTest extends BaseUnitTest {
         Assert.assertTrue(presenter instanceof FamilyCallDialogPresenter);
         assertNotNull(presenter);
     }
+
+    @Test
+    public void testSetPendingCallRequest() {
+        FamilyCallDialogContract.Dialer dialer = null;
+        fragment.setPendingCallRequest(dialer);
+        assertNull(fragment.getPendingCallRequest());
+    }
+
+    @Test
+    public void testSetFamilyBaseEntityId() {
+        fragment.setFamilyBaseEntityId("12345");
+        Assert.assertEquals("12345", fragment.familyBaseEntityId);
+    }
+
 
     @Test
     public void whenRefreshHeadOfFamilyViewAnswered() {
@@ -95,6 +106,20 @@ public class FamilyCallDialogFragmentTest extends BaseUnitTest {
         Assert.assertEquals("Family Head", model.getRole());
     }
 
+
+    @Test
+    public void whenRefreshHeadOfFamilyViewAnsweredWhenPhoneNumberIsNotBlank() {
+        Mockito.when(model.getPhoneNumber()).thenReturn("0727272828");
+        Mockito.when(model.getName()).thenReturn("Family head Name");
+        Mockito.when(model.getRole()).thenReturn("Family Head");
+        fragment.refreshHeadOfFamilyView(model);
+        Assert.assertNotNull(model);
+        Assert.assertEquals("0727272828", model.getPhoneNumber());
+        Assert.assertEquals("Family head Name", model.getName());
+        Assert.assertEquals("Family Head", model.getRole());
+    }
+
+
     @Test
     public void whenRefreshCareGiverViewAnswered() {
         Mockito.when(model.getName()).thenReturn("Care giver Name");
@@ -104,6 +129,43 @@ public class FamilyCallDialogFragmentTest extends BaseUnitTest {
         Assert.assertNull(model.getPhoneNumber());
         Assert.assertEquals("Care giver Name", model.getName());
         Assert.assertEquals("Care Giver", model.getRole());
+    }
+
+    @Test
+    public void whenRefreshCareGiverViewAnsweredWhenModelIsIndependent() {
+        Mockito.when(model.getName()).thenReturn("Care giver Name");
+        Mockito.when(model.getRole()).thenReturn("Care Giver");
+        Mockito.when(model.isIndependent()).thenReturn(true);
+        Pair<String, String> pair = new Pair<>(null, null);
+        Mockito.when(model.getOtherContact()).thenReturn(pair);
+
+        fragment.refreshCareGiverView(model);
+
+        Assert.assertNotNull(model);
+        Assert.assertNull(model.getPhoneNumber());
+        Assert.assertEquals("Care giver Name", model.getName());
+        Assert.assertEquals("Care Giver", model.getRole());
+        Assert.assertTrue(model.isIndependent());
+
+    }
+
+    @Test
+    public void whenRefreshCareGiverViewAnsweredWhenModelIsNotIndependent() {
+        Mockito.when(model.getPhoneNumber()).thenReturn("0727272828");
+        Mockito.when(model.getName()).thenReturn("Care giver Name");
+        Mockito.when(model.getRole()).thenReturn("Care Giver");
+        Mockito.when(model.isIndependent()).thenReturn(false);
+        Pair<String, String> pair = new Pair<>(null, null);
+        Mockito.when(model.getOtherContact()).thenReturn(pair);
+
+        fragment.refreshCareGiverView(model);
+
+        Assert.assertNotNull(model);
+        Assert.assertEquals("0727272828", model.getPhoneNumber());
+        Assert.assertEquals("Care giver Name", model.getName());
+        Assert.assertEquals("Care Giver", model.getRole());
+        Assert.assertFalse(model.isIndependent());
+
     }
 
 
