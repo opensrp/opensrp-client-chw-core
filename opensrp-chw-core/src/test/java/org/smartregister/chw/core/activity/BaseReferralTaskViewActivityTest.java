f1ec1c5b35f14249d677a541fef54d49a64f05b9
Allan O <a.onchuru@gmail.com>
2021-01-26 14:29:17 +0300

    :white_check_mark: Add getReferralDetails test
    Test correct details are set

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/BaseReferralTaskViewActivityTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/BaseReferralTaskViewActivityTest.java
@@ -2,7 +2,10 @@ package org.smartregister.chw.core.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,8 +19,11 @@ import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.chw.core.BaseUnitTest;
 import org.smartregister.chw.core.shadows.ShadowReferralTaskViewActivity;
 import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.commonregistry.CommonPersonObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.domain.Period;
 import org.smartregister.domain.Task;
+import org.smartregister.family.util.DBConstants;
 import org.smartregister.view.customcontrols.CustomFontTextView;
 
 import java.util.HashMap;
@@ -35,8 +41,7 @@ public class BaseReferralTaskViewActivityTest extends BaseUnitTest {
         intent.putExtra(CoreConstants.INTENT_KEY.USERS_TASKS, getTestTask());
         controller = Robolectric.buildActivity(ShadowReferralTaskViewActivity.class, intent).create().start();
         referralTaskViewActivity = controller.get();
-        Map<String, String> details = new HashMap<>();
-        CommonPersonObjectClient commonPersonObject = new CommonPersonObjectClient("case-id", details, "StoneField Mack");
+        CommonPersonObjectClient commonPersonObject = new CommonPersonObjectClient("case-id", new HashMap<>(), "StoneField Mack");
         BaseReferralTaskViewActivity.setPersonObjectClient(commonPersonObject);
     }
 
@@ -68,6 +73,44 @@ public class BaseReferralTaskViewActivityTest extends BaseUnitTest {
     }
 
 
+    @Test
+    public void getReferralDetailsSetsCorrectValues() {
+        CustomFontTextView clientName = Mockito.mock(CustomFontTextView.class);
+        CustomFontTextView referralDate = Mockito.mock(CustomFontTextView.class);
+        CustomFontTextView careGiverLayout = Mockito.mock(CustomFontTextView.class);
+        CustomFontTextView careGiverPhone = Mockito.mock(CustomFontTextView.class);
+        CustomFontTextView chwDetailsNames = Mockito.mock(CustomFontTextView.class);
+        LinearLayout womanGaLayout = Mockito.mock(LinearLayout.class);
+        ReflectionHelpers.setField(referralTaskViewActivity, "clientName", clientName);
+        ReflectionHelpers.setField(referralTaskViewActivity, "referralDate", referralDate);
+        ReflectionHelpers.setField(referralTaskViewActivity, "careGiverLayout", careGiverLayout);
+        ReflectionHelpers.setField(referralTaskViewActivity, "careGiverPhone", careGiverPhone);
+        ReflectionHelpers.setField(referralTaskViewActivity, "chwDetailsNames", chwDetailsNames);
+        ReflectionHelpers.setField(referralTaskViewActivity, "womanGaLayout", womanGaLayout);
+
+        ReflectionHelpers.setField(referralTaskViewActivity, "name", "StoneField Mack");
+        Map<String, String> columnMaps = new HashMap<>();
+        columnMaps.put(DBConstants.KEY.DOB, "01-01-2020");
+
+        CommonPersonObjectClient commonPersonObjectClient = new CommonPersonObjectClient("case-id", new HashMap<>(), "StoneField Mack");
+        commonPersonObjectClient.setColumnmaps(columnMaps);
+        BaseReferralTaskViewActivity.setPersonObjectClient(commonPersonObjectClient);
+
+
+        referralTaskViewActivity = Mockito.spy(referralTaskViewActivity);
+        referralTaskViewActivity.getReferralDetails();
+        Mockito.verify(referralTaskViewActivity, Mockito.times(1)).updateProblemDisplay();
+        Mockito.verify(clientName, Mockito.times(1)).setText(ArgumentMatchers.anyString());
+        Mockito.verify(referralDate, Mockito.times(1)).setText(ArgumentMatchers.anyString());
+        Mockito.verify(referralTaskViewActivity, Mockito.times(1)).getFamilyMemberContacts();
+        Mockito.verify(careGiverPhone, Mockito.times(1)).setText(ArgumentMatchers.anyString());
+        Mockito.verify(chwDetailsNames, Mockito.times(1)).setText(ArgumentMatchers.anyString());
+        Mockito.verify(referralTaskViewActivity, Mockito.times(1)).addGaDisplay();
+
+        Assert.assertEquals(View.GONE, careGiverLayout.getVisibility());
+    }
+
+
     public Task getTestTask() {
         Task task = new Task();
         task.setCode("192919-test-code");
@@ -75,6 +118,7 @@ public class BaseReferralTaskViewActivityTest extends BaseUnitTest {
         task.setLocation("some-location");
         task.setGroupIdentifier("some-group-id");
         task.setDescription("ANC Signs");
+        task.setExecutionPeriod(new Period(DateTime.now().minusDays(1), DateTime.now().plusDays(1)));
         return task;
     }
 
