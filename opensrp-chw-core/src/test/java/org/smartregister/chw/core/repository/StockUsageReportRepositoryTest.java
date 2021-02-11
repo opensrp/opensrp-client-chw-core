a19046026c6215140e6bf00a6494c39628a614f6
rkodev <43806892+rkodev@users.noreply.github.com>
2021-02-11 13:18:17 +0300

    Add tests

---
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/repository/StockUsageReportRepositoryTest.java
@@ -0,0 +1,80 @@
+package org.smartregister.chw.core.repository;
+
+import android.content.ContentValues;
+
+import net.sqlcipher.database.SQLiteDatabase;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.reflect.Whitebox;
+import org.smartregister.chw.core.BaseUnitTest;
+import org.smartregister.chw.core.domain.StockUsage;
+import org.smartregister.repository.Repository;
+import org.smartregister.view.activity.DrishtiApplication;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class StockUsageReportRepositoryTest extends BaseUnitTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    private StockUsageReportRepository stockUsageReportRepository;
+
+    @Mock
+    private static Repository repository;
+
+    @Mock
+    private SQLiteDatabase sqLiteDatabase;
+
+    @Captor
+    private ArgumentCaptor<ContentValues> contentValuesArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> stringArgumentCaptor;
+
+    @Before
+    public void setUp() {
+        stockUsageReportRepository = Mockito.spy(new StockUsageReportRepository());
+        when(repository.getReadableDatabase()).thenReturn(sqLiteDatabase);
+        when(repository.getWritableDatabase()).thenReturn(sqLiteDatabase);
+        Whitebox.setInternalState(DrishtiApplication.getInstance(), "repository", repository);
+    }
+
+    @Test
+    public void testAddOrUpdateShouldAdd() {
+
+        StockUsage stockUsage = new StockUsage();
+        stockUsage.setId("1234");
+        stockUsage.setStockName("West");
+        Mockito.doReturn(sqLiteDatabase).when(stockUsageReportRepository).getWritableDatabase();
+
+        stockUsageReportRepository.addOrUpdateStockUsage(stockUsage);
+
+        verify(sqLiteDatabase).replace(stringArgumentCaptor.capture(), stringArgumentCaptor.capture(), contentValuesArgumentCaptor.capture());
+        assertEquals(2, stringArgumentCaptor.getAllValues().size());
+
+        Iterator<String> iterator = stringArgumentCaptor.getAllValues().iterator();
+        assertEquals(StockUsageReportRepository.TABLE_NAME, iterator.next());
+        assertNull(iterator.next());
+
+        ContentValues contentValues = contentValuesArgumentCaptor.getValue();
+        assertEquals(8, contentValues.size());
+
+        assertEquals("1234", contentValues.getAsString("id"));
+        assertEquals("West", contentValues.getAsString("stock_name"));
+    }
+
+}
