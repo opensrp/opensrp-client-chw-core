bdd403bffb254062970305009b0186acd6382bfd
Qazi Abubakar <qma_2007@yahoo.com>
2021-03-30 15:38:31 +0500

    Added new test

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/utils/CoreChildUtilsTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/utils/CoreChildUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 import org.smartregister.chw.core.BaseUnitTest;
+import org.smartregister.chw.core.enums.ImmunizationState;
 
 public class CoreChildUtilsTest extends BaseUnitTest {
 
@@ -23,5 +24,11 @@ public class CoreChildUtilsTest extends BaseUnitTest {
     public void testGetImmunizationExpired() {
         String expectedStatus = "false";
         Assert.assertEquals(expectedStatus, CoreChildUtils.getImmunizationExpired("2019-09-10T19:00:00.000-05:00", "bcg"));
+    }
+
+    @Test
+    public void testGetDueStatus() {
+        ImmunizationState immunizationState = ImmunizationState.OVERDUE;
+        Assert.assertEquals(immunizationState, CoreChildUtils.getDueStatus("2019-09-10"));
     }
 }
