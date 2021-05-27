e291517a3af99df6d52ae7c16cfd29abdce4e1cb
hilpitome <hilpitome@gmail.com>
2021-05-27 09:11:55 +0300

    remove member test

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/interactor/CoreFamilyRemoveMemberInteractorTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/interactor/CoreFamilyRemoveMemberInteractorTest.java
@@ -3,51 +3,46 @@ package org.smartregister.chw.core.interactor;
 import android.os.Build;
 
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.powermock.reflect.Whitebox;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.annotation.Config;
-import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.Context;
 import org.smartregister.chw.core.application.CoreChwApplication;
-import org.smartregister.chw.core.application.TestApplication;
-import org.smartregister.chw.core.shadows.FamilyLibraryShadowUtil;
+import org.smartregister.chw.core.model.CommunityResponderModel;
 import org.smartregister.chw.core.shadows.UtilsShadowUtil;
-import org.smartregister.commonregistry.CommonPersonObject;
+import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.commonregistry.CommonRepository;
-import org.smartregister.family.FamilyLibrary;
+import org.smartregister.family.activity.FamilyWizardFormActivity;
 import org.smartregister.family.domain.FamilyMetadata;
 import org.smartregister.family.util.AppExecutors;
 import org.smartregister.chw.core.BaseUnitTest;
 import org.smartregister.chw.core.contract.FamilyRemoveMemberContract;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.smartregister.family.util.Utils;
+import org.smartregister.view.activity.BaseProfileActivity;
 import java.util.concurrent.Executor;
-
-import refutils.ReflectionHelper;
-
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
+@Config(shadows = {UtilsShadowUtil.class})
 public class CoreFamilyRemoveMemberInteractorTest extends BaseUnitTest implements Executor {
     private CoreFamilyRemoveMemberInteractor interactor;
-    @Mock
-    private  FamilyRemoveMemberContract.Presenter presenter;
-    @Mock
-    private FamilyLibrary familyLibrary;
-    @Mock
-    private FamilyMetadata metadata;
+
 
     @Mock
-    private Context context;
+    private  FamilyRemoveMemberContract.Presenter presenter;
+
     @Mock
     private CoreChwApplication coreApplication;
 
@@ -63,22 +58,6 @@ public class CoreFamilyRemoveMemberInteractorTest extends BaseUnitTest implement
     public void testRemoveMemberInteractorCallsMemberRemovedInPresenter(){
         interactor.removeMember("famId", "lastLocationId", new JSONObject(), presenter);
         verify(presenter).memberRemoved(null);
-    }
-
-    @Test
-    public void testProcessFamilyMemberCallsprocessMemberInPresenter(){
-
-        CommonPersonObjectClient  commonPersonObjectClient = Mockito.mock(CommonPersonObjectClient.class);
-        CommonRepository commonRepository = Mockito.mock(CommonRepository.class);
-        CommonPersonObject personObject = Mockito.mock(CommonPersonObject.class);
-        Mockito.doReturn(personObject).when(commonRepository).findByBaseEntityId(Mockito.anyString());
-        Mockito.doReturn(context).when(familyLibrary).context();
-        metadata.familyRegister = Mockito.mock(FamilyMetadata.FamilyRegister.class);
-        Whitebox.setInternalState(metadata.familyRegister, "tableName", "ec_family");
-
-        FamilyLibraryShadowUtil.setInstance(familyLibrary);
-        interactor.processFamilyMember("12345",commonPersonObjectClient, presenter);
-        verify(presenter).processMember(Mockito.any(), Mockito.any());
     }
 
 
