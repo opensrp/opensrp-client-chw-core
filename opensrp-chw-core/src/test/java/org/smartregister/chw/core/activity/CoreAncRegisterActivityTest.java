dc60f02c12bad3f5eef60de91d2f98fded974967
qaziabubakar-vd <qaziabubakar47@gmail.com>
2021-08-16 16:43:37 +0500

    removed unused import from CoreAncRegisterActivityTest

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreAncRegisterActivityTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreAncRegisterActivityTest.java
@@ -1,7 +1,6 @@
 package org.smartregister.chw.core.activity;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
