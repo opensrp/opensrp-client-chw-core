7e855a5330e1b7a0f3fe537d9d185d1d4ee0e000
Allan O <a.onchuru@gmail.com>
2021-08-20 18:30:30 +0300

    :white_check_mark: Add CoreFamilyPlanningMemberProfileActivity tests
    Test views and presenter initialized correctly

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreFamilyPlanningMemberProfileActivityTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/activity/CoreFamilyPlanningMemberProfileActivityTest.java
@@ -1,19 +1,35 @@
 package org.smartregister.chw.core.activity;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import android.content.Intent;
+import android.widget.TextView;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.Robolectric;
 import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.chw.core.BaseUnitTest;
+import org.smartregister.chw.core.R;
 import org.smartregister.chw.core.activity.impl.CoreFamilyPlanningMemberProfileActivityImpl;
+import org.smartregister.chw.core.presenter.CoreFamilyPlanningProfilePresenter;
+import org.smartregister.chw.core.shadows.FPDaoShadowHelper;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.fp.domain.FpMemberObject;
+import org.smartregister.chw.fp.util.FamilyPlanningConstants;
 
+
+@Config(shadows = FPDaoShadowHelper.class)
 public class CoreFamilyPlanningMemberProfileActivityTest extends BaseUnitTest {
 
     @Rule
@@ -21,22 +37,38 @@ public class CoreFamilyPlanningMemberProfileActivityTest extends BaseUnitTest {
 
     private CoreFamilyPlanningMemberProfileActivityImpl activity;
     private ActivityController<CoreFamilyPlanningMemberProfileActivityImpl> controller;
-
+    private FpMemberObject fpMemberObject;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        Intent intent = new Intent();
-        controller = Robolectric.buildActivity(CoreFamilyPlanningMemberProfileActivityImpl.class, intent).create().start();
-        activity = controller.get();
+        fpMemberObject = new FpMemberObject();
+        fpMemberObject.setFamilyName("Tester");
+        fpMemberObject.setFpMethod(FamilyPlanningConstants.DBConstants.FP_COC);
+        fpMemberObject.setFpStartDate("2021-08-01");
+        fpMemberObject.setBaseEntityId("test-member-123");
 
-        /*Context context = Context.getInstance();
-        CoreLibrary.init(context);*/
+        Intent intent = new Intent();
+        intent.putExtra(FamilyPlanningConstants.FamilyPlanningMemberObject.MEMBER_OBJECT, fpMemberObject);
+        intent.putExtra(CoreConstants.INTENT_KEY.TOOLBAR_TITLE, R.string.return_to_family_name);
+        controller = Robolectric.buildActivity(CoreFamilyPlanningMemberProfileActivityImpl.class, intent).create().start();
+        activity = Mockito.spy(controller.get());
+        activity.onCreation();
     }
 
     @Test
-    public void onCreationInitsCorrectViews() {
+    public void onCreationInitsViews() {
+        TextView toolbarTextView = activity.findViewById(R.id.toolbar_title);
+        Assert.assertEquals(toolbarTextView.getText(), activity.getString(R.string.return_to_family_name, "Tester"));
+        assertNotNull(ReflectionHelpers.getField(activity, "notificationAndReferralLayout"));
+        assertNotNull(ReflectionHelpers.getField(activity, "notificationAndReferralRecyclerView"));
+    }
 
+
+    @Test
+    public void presenterIsInitializedCorrectly() {
+        assertNotNull(ReflectionHelpers.getField(activity, "fpProfilePresenter"));
+        assertTrue(ReflectionHelpers.getField(activity, "fpProfilePresenter") instanceof CoreFamilyPlanningProfilePresenter);
     }
 
     @After
