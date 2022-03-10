c3edcbd13b627295bce58c8edd8e080505f73f1d
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-03-10 11:41:19 +0300

    Refactored isClientKnownOnArt implementation in CorePmtctDao

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/dao/CorePmtctDao.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/dao/CorePmtctDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class CorePmtctDao extends PmtctDao {
     public static boolean isClientKnownOnArt(String baseEntityId){
-        String sql = "SELECT known_on_art FROM ec_pmtct_followup p " +
+        String sql = "SELECT known_on_art FROM ec_pmtct_registration p " +
                 "WHERE p.base_entity_id = '" + baseEntityId + "'";
 
         DataMap<String> dataMap = cursor -> getCursorValue(cursor, "known_on_art");
