ead4f11ff1e7fe99b5b5ddbb0efecda9a0897c3b
hilpitome <hilpitome@gmail.com>
2022-03-15 13:08:06 +0300

    test familyNameAndPhone method

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/repository/MalariaRegisterRepositoryTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/repository/MalariaRegisterRepositoryTest.java
@@ -1,12 +1,18 @@
 package org.smartregister.chw.core.repository;
 
+import net.sqlcipher.Cursor;
 import net.sqlcipher.database.SQLiteDatabase;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.smartregister.chw.anc.util.Constants;
 import org.smartregister.chw.core.BaseRobolectricTest;
+
+import java.util.HashMap;
 
 public class MalariaRegisterRepositoryTest extends BaseRobolectricTest {
 
@@ -24,6 +30,19 @@ public class MalariaRegisterRepositoryTest extends BaseRobolectricTest {
 
     @Test
     public void testGetFamilyNameAndPhoneWithBaseEntityIdReturnsCorrectDetails(){
+
+        String correctBaseEntityId = "4b3e6408-0549-470a-b24a-82ac71180a30";
+        String nonExistentRecordBaseEntityId = "adbc49cc-f0f2-42c2-a53f-634983922eb0";
+
+        HashMap<String, String> results = new HashMap<>();
+        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_NAME, "Kibaki");
+        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_PHONE, "9938383");
+
+        Mockito.doReturn(results).when(malariaRegisterRepository).getFamilyNameAndPhone(correctBaseEntityId);
+
+        Assert.assertEquals(results, malariaRegisterRepository.getFamilyNameAndPhone(correctBaseEntityId));
+        Assert.assertNull(null, malariaRegisterRepository.getFamilyNameAndPhone(nonExistentRecordBaseEntityId));
+
 
     }
 }
