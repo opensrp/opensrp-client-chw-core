ca4f3e5a253daaf06be7f18a1f60f974741e0368
hilpitome <hilpitome@gmail.com>
2022-03-22 12:37:20 +0300

    test correct arguments are passed to db query

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/repository/MalariaRegisterRepositoryTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/repository/MalariaRegisterRepositoryTest.java
@@ -2,22 +2,41 @@ package org.smartregister.chw.core.repository;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
+import org.apache.commons.validator.Arg;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.smartregister.chw.anc.util.Constants;
 import org.smartregister.chw.core.BaseRobolectricTest;
+import org.smartregister.chw.core.utils.CoreConstants;
 
 import java.util.HashMap;
 
 public class MalariaRegisterRepositoryTest extends BaseRobolectricTest {
-
+    @Mock
     private MalariaRegisterRepository malariaRegisterRepository;
 
     @Mock
     private SQLiteDatabase database;
+
+    @Captor
+    ArgumentCaptor<String> tableNameCaptor;
+    @Captor
+    ArgumentCaptor<String[]> tableColumnsCaptor;
+    @Captor
+    ArgumentCaptor<String> selectionCaptor;
+    @Captor
+    ArgumentCaptor<String[]> selectionArgsCaptor;
+    @Captor
+    ArgumentCaptor<String> groupByCaptor;
+    @Captor
+    ArgumentCaptor<String> havingCaptor;
+    @Captor
+    ArgumentCaptor<String> orderByCaptor;
 
 
     @Before
@@ -27,20 +46,11 @@ public class MalariaRegisterRepositoryTest extends BaseRobolectricTest {
     }
 
     @Test
-    public void testGetFamilyNameAndPhoneWithBaseEntityIdReturnsCorrectDetails(){
-
-        String correctBaseEntityId = "4b3e6408-0549-470a-b24a-82ac71180a30";
-        String nonExistentRecordBaseEntityId = "adbc49cc-f0f2-42c2-a53f-634983922eb0";
-
-        HashMap<String, String> results = new HashMap<>();
-        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_NAME, "Kibaki");
-        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_PHONE, "9938383");
-
-        Mockito.doReturn(results).when(malariaRegisterRepository).getFamilyNameAndPhone(correctBaseEntityId);
-
-        Assert.assertEquals(results, malariaRegisterRepository.getFamilyNameAndPhone(correctBaseEntityId));
-        Assert.assertNull(null, malariaRegisterRepository.getFamilyNameAndPhone(nonExistentRecordBaseEntityId));
-
-
+    public void testDatabaseQueryIsCreatedWithTheCorrectArgumentInGetFamilyNamePhone(){
+        String baseEntityId = "4b3e6408-0549-470a-b24a-82ac71180a30";
+        malariaRegisterRepository.getFamilyNameAndPhone(baseEntityId);
+        Mockito.verify(database).query(tableNameCaptor.capture(), tableColumnsCaptor.capture(), selectionCaptor.capture(), selectionArgsCaptor.capture(),
+                groupByCaptor.capture(), havingCaptor.capture(), orderByCaptor.capture());
+        Assert.assertEquals(MalariaRegisterRepository.TABLE_NAME, tableNameCaptor.getValue());
     }
 }
