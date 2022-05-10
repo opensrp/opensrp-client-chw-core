bfe1a43d826fb0a244cc9f57f4bd3bbd75c0647f
Allan <a.onchuru@gmail.com>
2022-05-10 11:23:12 +0300

    :white_check_mark: Fix failing tests

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/shadows/FormUtilsShadowHelper.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/shadows/FormUtilsShadowHelper.java
@@ -2,6 +2,7 @@ package org.smartregister.chw.core.shadows;
 
 import android.content.Context;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
