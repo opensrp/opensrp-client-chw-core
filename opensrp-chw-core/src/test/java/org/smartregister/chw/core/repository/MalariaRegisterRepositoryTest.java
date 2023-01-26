0aab92e7e132a1aa904b7a84b985f8782bf25780
hilpitome <hilpitome@gmail.com>
2023-01-26 11:55:25 +0300

    updare testGetFamilyNameAndPhoneWithBaseEntityIdReturnsCorrectDetails

--- opensrp-chw-core/src/test/java/org/smartregister/chw/core/repository/MalariaRegisterRepositoryTest.java
+++ opensrp-chw-core/src/test/java/org/smartregister/chw/core/repository/MalariaRegisterRepositoryTest.java
@@ -2,6 +2,7 @@ package org.smartregister.chw.core.repository;
 
 import static org.smartregister.repository.BaseRepository.COLLATE_NOCASE;
 
+import net.sqlcipher.MatrixCursor;
 import net.sqlcipher.database.SQLiteDatabase;
 
 import org.junit.Assert;
@@ -15,6 +16,7 @@ import org.smartregister.chw.anc.util.Constants;
 import org.smartregister.chw.core.BaseRobolectricTest;
 
 import java.util.HashMap;
+import org.smartregister.chw.core.domain.FamilyMember;
 
 public class MalariaRegisterRepositoryTest extends BaseRobolectricTest {
     @Mock
@@ -64,17 +66,30 @@ public class MalariaRegisterRepositoryTest extends BaseRobolectricTest {
     public void testGetFamilyNameAndPhoneWithBaseEntityIdReturnsCorrectDetails(){
 
         String correctBaseEntityId = "4b3e6408-0549-470a-b24a-82ac71180a30";
-        String nonExistentRecordBaseEntityId = "adbc49cc-f0f2-42c2-a53f-634983922eb0";
 
         HashMap<String, String> results = new HashMap<>();
-        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_NAME, "Kibaki");
-        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_PHONE, "9938383");
+        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_NAME, "Emilio Mwai Kibaki");
+        results.put(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_PHONE, "0799938383");
 
-        Mockito.doReturn(results).when(malariaRegisterRepository).getFamilyNameAndPhone(correctBaseEntityId);
+        MatrixCursor mockCursor =  new MatrixCursor(new String[]
+                {
+                        MalariaRegisterRepository.FIRST_NAME,
+                        MalariaRegisterRepository.LAST_NAME,
+                        MalariaRegisterRepository.MIDDLE_NAME,
+                        MalariaRegisterRepository.PHONE_NUMBER
+                });
+
+        mockCursor.newRow()
+                .add("Emilio")
+                .add("Kibaki")
+                .add("Mwai")
+                .add("0799938383");
+
+        Mockito.doReturn(mockCursor).when(database).query(Mockito.any(), Mockito.any(), Mockito.any(),
+                Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
 
         Assert.assertEquals(results, malariaRegisterRepository.getFamilyNameAndPhone(correctBaseEntityId));
-        Assert.assertNull(null, malariaRegisterRepository.getFamilyNameAndPhone(nonExistentRecordBaseEntityId));
-
 
     }
+
 }
